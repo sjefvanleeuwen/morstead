a4c70aa6fa1945f9d7ae3ec84397a1a7e50296ea
sjefvanleeuwen <@sjefvanleeuwen>
2019-12-30 00:45:53 +0100

    Fixing Thread Culture Issue.

--- Vs.VoorzieningenEnRegelingen.Core.Tests/TimePeriodTests.cs
+++ Vs.VoorzieningenEnRegelingen.Core.Tests/TimePeriodTests.cs
@@ -10,13 +10,14 @@ namespace Vs.VoorzieningenEnRegelingen.Core.Tests
         [Fact]
         public void TimePeriod_Can_Serialize()
         {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
             var range = new Itenso.TimePeriod.TimeRange();
             // range.m
 
             // "01/01/0001 00:00:00 - 31/12/9999 23:59:59 | 3652058.23:59"
             var s = range.ToString();
 
-            var dateTimeStrings = s.Split('|')[0].Split('-');
+            var dateTimeStrings = s.Split('|')[0].Split(" - ");
 
             var l = new TimeRange(DateTime.Parse(dateTimeStrings[0],new CultureInfo("nl-NL")), DateTime.Parse(dateTimeStrings[1],new CultureInfo("nl-NL")));
         }
