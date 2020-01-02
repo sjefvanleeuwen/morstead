c863bff5632a841f69ec11b79759491647617f64
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-02 11:02:38 +0100

    added Session Repository

---
+++ Vs.VoorzieningenEnRegelingen.Core.Tests/SessionRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Vs.VoorzieningenEnRegelingen.Core.Model;
+using Xunit;
+
+namespace Vs.VoorzieningenEnRegelingen.Core.Tests
+{
+    public class SessionRepositoryTests
+    {
+
+        [Fact]
+        public void Persistence_Load_Configuration()
+        {
+            var sessionRepository = new SessionRepository(new TimeSpan(0, 15, 0));
+            var sessionId = Guid.NewGuid();
+            var parameters = new ParametersCollection() {
+                new Parameter("alleenstaande","ja"),
+                new Parameter("woonland","Nederland"),
+                new Parameter("toetsingsinkomen_aanvrager",(double)19000),
+                new Parameter("toetsingsinkomen_toeslagpartner",(double)0)
+            };
+
+            sessionRepository.Persist(sessionId, parameters);
+            ParametersCollection result = sessionRepository.Retrieve(sessionId);
+            Assert.False(result == null);
+            Assert.True(result.Count == 4);
+        }
+    }
+}
---
+++ Vs.VoorzieningenEnRegelingen.Core/SessionRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Vs.VoorzieningenEnRegelingen.Core
+{
+    public class SessionRepository
+    {
+        private readonly IMemoryCache _cache;
+
+        public TimeSpan SessionTimeOut { get; }
+        public ServiceProvider _provider { get; }
+
+        public SessionRepository(TimeSpan sessionTimeOut)
+        {
+            // make sure backend cache is still valid before sliding expiration on clients.
+            SessionTimeOut = sessionTimeOut.Add(new TimeSpan(0,1,0));
+            _provider = new ServiceCollection()
+                .AddMemoryCache()
+                .BuildServiceProvider();
+
+            _cache = _provider.GetService<IMemoryCache>();
+        }
+
+        public void Persist(Guid sessionId, ParametersCollection parameters)
+        {
+            using (var entry = _cache.CreateEntry(sessionId))
+            {
+                entry.Value = parameters;
+                entry.SlidingExpiration = SessionTimeOut;
+            }
+        }
+
+        public ParametersCollection Retrieve(Guid sessionId)
+        {
+            ParametersCollection result;
+            _cache.TryGetValue(sessionId, out result);
+            return result;
+        }
+    }
+}
