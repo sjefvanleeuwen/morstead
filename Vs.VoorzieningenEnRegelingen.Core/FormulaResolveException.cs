3bd98634bb02cea424747aeec677947335c44e5a
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-02 11:13:36 +0100

    Adding not implemented exception constraints on stepException

--- Vs.VoorzieningenEnRegelingen.Core/FormulaResolveException.cs
+++ Vs.VoorzieningenEnRegelingen.Core/FormulaResolveException.cs
@@ -15,14 +15,17 @@ namespace Vs.VoorzieningenEnRegelingen.Core
 
         public StepException()
         {
+            throw new NotImplementedException();
         }
 
         public StepException(string message) : base(message)
         {
+            throw new NotImplementedException();
         }
 
         public StepException(string message, Exception innerException) : base(message, innerException)
         {
+            throw new NotImplementedException();
         }
 
         protected StepException(System.Runtime.Serialization.SerializationInfo serializationInfo, System.Runtime.Serialization.StreamingContext streamingContext)
