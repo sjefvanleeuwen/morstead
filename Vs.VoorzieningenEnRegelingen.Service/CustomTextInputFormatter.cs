5efd08ee3fd0dade11180033bc4a393bbe7d37bd
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-05 14:46:25 +0100

    Adding Swagger

---
+++ Vs.VoorzieningenEnRegelingen.Service/CustomTextInputFormatter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vs.VoorzieningenEnRegelingen.Service
+{
+    public class CustomTextInputFormatter : TextInputFormatter
+    {
+        // Required to use text/html or foo/bar in ConsumesAttribute, see ConsumesController and ConsumesTests
+
+        public CustomTextInputFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/html"));
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("foo/bar"));
+
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+        {
+            return Task.FromResult(InputFormatterResult.Success(null));
+        }
+    }
+}
---
+++ Vs.VoorzieningenEnRegelingen.Service/CustomTextOutputFormatter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vs.VoorzieningenEnRegelingen.Service
+{
+    public class CustomTextOutputFormatter : TextOutputFormatter
+    {
+        // Required to use text/html or foo/bar in ProducesAttribute, see ProducesController and ProducesTests
+
+        public CustomTextOutputFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/html"));
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("foo/bar"));
+
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
