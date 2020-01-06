207265aa4ef76245a0c2f56dae58a77ced464cac
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-06 01:29:58 +0100

    Styling website, WIP

--- Vs.VoorzieningenEnRegelingen.Site/Startup.cs
+++ Vs.VoorzieningenEnRegelingen.Site/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Vs.VoorzieningenEnRegelingen.Site.Data;
 
 namespace Vs.VoorzieningenEnRegelingen.Site
 {
@@ -22,7 +21,6 @@ namespace Vs.VoorzieningenEnRegelingen.Site
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
