935807589d2b458519faece03af31c4e45b91b3b
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-08 00:34:48 +0100

    Adding Ace Editor (YAML mode) and Blazor JSInterop with it.

--- Vs.VoorzieningenEnRegelingen.Site/Startup.cs
+++ Vs.VoorzieningenEnRegelingen.Site/Startup.cs
@@ -20,7 +20,8 @@ namespace Vs.VoorzieningenEnRegelingen.Site
         public static void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddServerSideBlazor()
+                .AddCircuitOptions(options => { options.DetailedErrors = true; }); // For detailed JS Interop debugging in browser.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Vs.VoorzieningenEnRegelingen.Site/wwwroot/js/ace.interop.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="ace.min.js" />
+
+function createEditor() {
+    ace.config.set('basePath', '/js/ace');
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chaos");
+    editor.session.setMode("ace/mode/yaml");
+}
