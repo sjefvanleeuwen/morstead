a81549d03a99c680578858f5945a0fd66c382c99
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-23 12:17:47 +0100

    Security layer (OpenIdConnect) and sign in flow / authorized views

--- Vs.VoorzieningenEnRegelingen.Site/Startup.cs
+++ Vs.VoorzieningenEnRegelingen.Site/Startup.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Vs.VoorzieningenEnRegelingen.Site
 {
@@ -10,10 +15,12 @@ namespace Vs.VoorzieningenEnRegelingen.Site
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("auth0.json")
+                .Build();
         }
 
-        public IConfiguration Configuration { get; }
+        private static IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -22,6 +29,70 @@ namespace Vs.VoorzieningenEnRegelingen.Site
             services.AddRazorPages();
             services.AddServerSideBlazor()
                 .AddCircuitOptions(options => { options.DetailedErrors = true; }); // For detailed JS Interop debugging in browser.
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            // Add authentication services
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect("Auth0", options =>
+            {
+      // Set the authority to your Auth0 domain
+      options.Authority = $"https://{Configuration["Auth0:Domain"].ToString()}";
+
+      // Configure the Auth0 Client ID and Client Secret
+      options.ClientId = Configuration["Auth0:ClientId"];
+                options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+      // Set response type to code
+      options.ResponseType = "code";
+
+      // Configure the scope
+      options.Scope.Clear();
+                options.Scope.Add("openid");
+
+      // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+      // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+      options.CallbackPath = new PathString("/callback");
+
+      // Configure the Claims Issuer to be Auth0
+      options.ClaimsIssuer = "Auth0";
+
+       options.Events = new OpenIdConnectEvents
+                {
+          // handle the logout redirection
+          OnRedirectToIdentityProviderForSignOut = (context) =>
+                    {
+                        var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                        var postLogoutUri = context.Properties.RedirectUri;
+                        if (!string.IsNullOrEmpty(postLogoutUri))
+                        {
+                            if (postLogoutUri.StartsWith("/"))
+                            {
+                      // transform to absolute
+                      var request = context.Request;
+                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                            }
+                            logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                        }
+
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +113,9 @@ namespace Vs.VoorzieningenEnRegelingen.Site
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
