0f38c2f74e116001b6ef92985efcb7d248a2026d
sjefvanleeuwen <@sjefvanleeuwen>
2020-01-25 23:27:38 +0100

    Making User Profile more dynamic (WIP)

--- Vs.VoorzieningenEnRegelingen.Site/Model/UserProfile.cs
+++ Vs.VoorzieningenEnRegelingen.Site/Model/UserProfile.cs
@@ -1,21 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Vs.VoorzieningenEnRegelingen.Site.Shared;
+﻿using System.Collections.Generic;
 
 namespace Vs.VoorzieningenEnRegelingen.Site.Model
 {
-    [Authorize]
     public class UserProfile
     {
         public string EmailAddress { get; set; }
         public string Name { get; set; }
-        public string ProfileImage { get; set; }
+        public string ImageUrl { get; set; }
+        public string Occupation { get; set; }
+        public long Followers { get; set; }
+        public long Colleagues { get; set; }
+        public long Following { get; set; }
+        public string Education { get; set; }
+        public string Municipality { get; set; }
+        public IEnumerable<string> Skills { get; set; }
+        public string Info { get; set; }
 
-
+        public UserProfile()
+        {
+ 
+        }
     }
-
 }
