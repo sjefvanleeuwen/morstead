24feed14932d77829bb9d042bfc1749b584388ee
sjefvanleeuwen <@sjefvanleeuwen>
2020-02-11 21:43:42 +0100

    Adding Cms Project, update dependencies, initial integration test.

---
+++ cms/Vs.Cms.GitHubStorage/FileStorageOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vs.Cms.GitHubFileStorage
+{
+    public class FileStorageOptions
+    {
+        public string ApiKey { get; set; }
+        public string Repository { get; set; }
+        public string User { get; set; }
+        public string Product { get; set; }
+    }
+}
---
+++ cms/Vs.Cms.GitHubStorage/GitHubFileStorageService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vs.Cms.GitHubFileStorage
+{
+    public class GitHubFileStorageService : IGitHubFileStorageService
+    {
+    }
+
+    public interface IGitHubFileStorageService
+    {
+    }
+}
