50144ec012fd0365b2492e9083702f223161e89e
sjefvanleeuwen <@sjefvanleeuwen>
2020-02-15 22:05:38 +0100

    Adding schema package and storage service

---
+++ graph/Vs.Graph.Core/SchemaPackageServices/InMemorySchemaPackageService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Vs.Graph.Core.SchemaPackageServices
+{
+    public class InMemorySchemaPackageService : ISchemaPackageStorageService
+    {
+        public InMemorySchemaPackageService()
+        {
+            Files = new Dictionary<string, string>();
+        }
+
+        public Dictionary<string,string> Files { get; set; }
+
+        public IEnumerable<KeyValuePair<string,string>> GetFiles(string migrationUrn) =>
+            Files.Where(p => p.Key.StartsWith(migrationUrn));
+
+        public string Load(string urn)
+        {
+            return Files[urn];
+        }
+    }
+}
