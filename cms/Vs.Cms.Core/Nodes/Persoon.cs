40e3b29ddf29230ca30143b11ffcf1e37f30daef
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-02-18 21:04:29 +0000

    Adding Cms Nodes

---
+++ cms/Vs.Cms.Core/Nodes/Persoon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Vs.Graph.Core.Data;
+
+namespace Vs.Cms.Core.Nodes
+{
+    public class Persoon : INode, IPeriode
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public DateTime PeriodeBegin { get; set; }
+        public DateTime PeriodeEind { get; set; }
+    }
+}
---
+++ cms/Vs.Cms.Core/Nodes/Publicatie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Vs.Graph.Core.Data;
+
+namespace Vs.Cms.Core
+{
+    public class Publicatie : INode, IMoment
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
