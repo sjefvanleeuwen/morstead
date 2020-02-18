f4cb6d8ed7180bcaacde063ea3607db09133049a
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-02-18 21:04:42 +0000

    Adding Cms Edges

---
+++ cms/Vs.Cms.Core/Edges/Akkordeert.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Vs.Graph.Core.Data;
+
+namespace Vs.Cms.Core.Edges
+{
+    public class Akkordeert : IEdge, IMoment
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+    }
+}
---
+++ cms/Vs.Cms.Core/Edges/Reviewed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Vs.Graph.Core.Data;
+
+namespace Vs.Cms.Core.Edges
+{
+    public class Reviewed : IEdge, IPeriode
+    {
+        public DateTime PeriodeBegin { get; set; }
+        public DateTime PeriodeEind { get; set; }
+        public int Id { get; set; }
+    }
+}
