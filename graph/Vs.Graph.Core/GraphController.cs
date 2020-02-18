3ff524d62aba69af0e1953a2ede84b6be21fe309
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-02-18 21:03:16 +0000

    Graph controller (in progress)

---
+++ graph/Vs.Graph.Core/GraphController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Vs.Graph.Core.Data;
+
+namespace Vs.Graph.Core
+{
+    public class GraphController : IGraphController
+    {
+        public bool DeleteAllEdges<T>() where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAllNodes<T>() where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteEdge<T>(T obj) where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteEdges<T>(IEnumerable<T> list) where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteNode<T>(T obj) where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteNodes<T>(IEnumerable<T> list) where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAllEdges<T>() where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAllNodes<T>() where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetEdge<T>(int id) where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetNode<T>(int id) where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertEdge<T,TNode>(T obj, TNode van, TNode naar) where T : IEdge where TNode : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertEdges<T>(IEnumerable<T> list) where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertNode<T>(T obj) where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertNodes<T>(IEnumerable<T> list) where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEdge<T>(T obj) where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEdges<T>(IEnumerable<T> list) where T : IEdge
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNode<T>(T obj) where T : INode
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNodes<T>(IEnumerable<T> list) where T : INode
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ graph/Vs.Graph.Core/IGraphController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Vs.Graph.Core.Data;
+
+namespace Vs.Graph.Core
+{
+    public interface IGraphController
+    {
+        T GetNode<T>(int id) where T : INode;
+        IEnumerable<T> GetAllNodes<T>() where T : INode;
+        int InsertNode<T>(T obj) where T : INode;
+        int InsertNodes<T>(IEnumerable<T> list) where T : INode;
+        bool UpdateNode<T>(T obj) where T : INode;
+        bool UpdateNodes<T>(IEnumerable<T> list) where T : INode;
+        bool DeleteNode<T>(T obj) where T : INode;
+        bool DeleteNodes<T>(IEnumerable<T> list) where T : INode;
+        bool DeleteAllNodes<T>() where T : INode;
+        T GetEdge<T>(int id) where T : IEdge;
+        IEnumerable<T> GetAllEdges<T>() where T : IEdge;
+        int InsertEdge<T, TNode>(T obj, TNode van, TNode naar) where T : IEdge where TNode : INode;
+        int InsertEdges<T>(IEnumerable<T> list) where T : IEdge;
+        bool UpdateEdge<T>(T obj) where T : IEdge;
+        bool UpdateEdges<T>(IEnumerable<T> list) where T : IEdge;
+        bool DeleteEdge<T>(T obj) where T : IEdge;
+        bool DeleteEdges<T>(IEnumerable<T> list) where T : IEdge;
+        bool DeleteAllEdges<T>() where T : IEdge;
+    }
+}
