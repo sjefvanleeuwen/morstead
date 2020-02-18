7e67642ba21b451400fa4041da7c99a94220a5e2
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-02-18 21:14:00 +0000

    Creating setter

--- cms/Vs.Cms.Core/Nodes/Publicatie.cs
+++ cms/Vs.Cms.Core/Nodes/Publicatie.cs
@@ -6,6 +6,6 @@ namespace Vs.Cms.Core
     public class Publicatie : INode, IMoment
     {
         public int Id { get; set; }
-        public DateTime Moment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime Moment { get; set; }
     }
 }
