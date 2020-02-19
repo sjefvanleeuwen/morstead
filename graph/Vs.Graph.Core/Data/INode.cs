a210fdfe679f60e98688c1755345679d11b954c3
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-02-19 21:29:23 +0000

    Timeperiod for node

--- graph/Vs.Graph.Core/Data/INode.cs
+++ graph/Vs.Graph.Core/Data/INode.cs
@@ -1,7 +1,10 @@
-﻿namespace Vs.Graph.Core.Data
+﻿using Itenso.TimePeriod;
+
+namespace Vs.Graph.Core.Data
 {
     public interface INode
     {
         int Id { get; set; }
+        TimeRange Periode { get; set; }
     }
 }
