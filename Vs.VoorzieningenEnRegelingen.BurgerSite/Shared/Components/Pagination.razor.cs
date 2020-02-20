31ff8ad06d4e8d1d05d983fd3ffadf4ae34de386
marijn@pessers.net <marijn@pessers.net>
2020-02-20 11:29:32 +0100

    All elements made into components. Overrode header css to match with Virtual Society icon.

---
+++ Vs.VoorzieningenEnRegelingen.BurgerSite/Shared/Components/Pagination.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Vs.VoorzieningenEnRegelingen.BurgerSite.Shared.Components
+{
+    public partial class Pagination
+    {
+        [Parameter]
+        public string Next { get; set; }
+        [Parameter]
+        public string NextText { get; set; }
+        [Parameter]
+        public string Previous { get; set; }
+        [Parameter]
+        public string PreviousText { get; set; }
+        [Parameter]
+        public string ScreenreaderDescription { get; set; }
+    }
+}
