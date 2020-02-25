0290c4156afef1a950acaa2c3e768f9118f8a136
Marijn Pessers <marijn@pessers.net>
2020-02-25 23:35:16 +0100

    FormElements written out to separate components

---
+++ Vs.VoorzieningenEnRegelingen.BurgerPortaal/Shared/Components/FormElements/Radio.razor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Shared.Components.FormElements
+{
+    public partial class Radio
+    {
+        private bool _isHorizontalRadio => Options.Count == 2 && Options.All(o => o.Value.Length <= 10);
+        protected IEnumerable<string> _keys => Options.Keys;
+    }
+}
