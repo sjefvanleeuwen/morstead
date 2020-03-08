e22773af5d550b65c7311a938953cb8ac010236f
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-03-08 15:10:12 +0100

    Resolve To Questions Unit Test for generating semantic content key links WIP

--- Vs.VoorzieningenEnRegelingen.Core.Tests/ModelTests.cs
+++ Vs.VoorzieningenEnRegelingen.Core.Tests/ModelTests.cs
@@ -1,5 +1,10 @@
-﻿using Vs.VoorzieningenEnRegelingen.Core.Tests.YamlScripts;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vs.VoorzieningenEnRegelingen.Core.Model;
+using Vs.VoorzieningenEnRegelingen.Core.Tests.YamlScripts;
 using Xunit;
+using static Vs.VoorzieningenEnRegelingen.Core.TypeInference.InferenceResult;
 
 namespace Vs.VoorzieningenEnRegelingen.Core.Tests
 {
@@ -29,6 +34,82 @@ namespace Vs.VoorzieningenEnRegelingen.Core.Tests
             Assert.True(header.Status == "ontwikkel");
             Assert.True(header.Jaar == "2019");
             Assert.True(header.Bron == "https://download.belastingdienst.nl/toeslagen/docs/berekening_zorgtoeslag_2019_tg0821z91fd.pdf");
+        }
+
+
+        /// <summary>
+        /// Resolve to question parameter or constant through recursion for a situation.
+        /// </summary>
+        /// <param name="expression"></param>
+        private void ResolveToQuestion(ref YamlScriptController controller, Model.Model model, string name, ref List<ContentNode> items, string situation = null)
+        {
+            var formula = model.Formulas.Find(p => p.Name == name);
+            if (formula == null)
+                return;
+            // resolve all situations.
+            foreach (var function in formula.Functions)
+            {
+                // Get Questions for function
+                var parameters = controller.GetFunctionTree(controller, function.Expression);
+                var nodeName = string.Join('.', new[] { situation, function.Situation, name }.Where(s => !string.IsNullOrEmpty(s)));
+                // Get Boolean Question for situation (if not previously asked)
+                if (function.IsSituational && items.Find(p => p.Name == nodeName) == null /* distinct */)
+                {
+                    items.Add(new ContentNode(nodeName) { IsSituational = function.IsSituational, Situation = function.Situation, Parameter = new Parameter(function.Situation, false, TypeEnum.Boolean, ref model) });
+                }
+                foreach (var parameter in parameters)
+                {
+                    if (model.Formulas.Find(p => p.Name == parameter.Name) == null)
+                    {
+                        // not a formula name, so it resolves to a question, add it to the list
+                        items.Add(new ContentNode(string.Join('.', new[] { parameter.Name, function.Situation, name }.Where(s => !string.IsNullOrEmpty(s)))) { IsSituational = function.IsSituational, Situation = function.Situation, Parameter = parameter });
+                        // find formula's that use the answer to this question.
+                        ResolveToQuestion(ref controller, model, parameter.Name, ref items);
+                    }
+                    else
+                    {
+                        // Recurse to find next question.
+                        ResolveToQuestion(ref controller, model, parameter.Name, ref items);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void BuildTreeForContent()
+        {
+            List<ContentNode> items = new List<ContentNode>();
+            var controller = new YamlScriptController();
+            var result = controller.Parse(YamlZorgtoeslag3.Body);
+            var model = result.Model;
+            Assert.False(result.IsError);
+            ContentNode tree = new ContentNode(result.Model.Header.Onderwerp);
+            var questions = controller.GetFunctionTree(controller);
+            foreach (var step in result.Model.Steps)
+            {
+                ResolveToQuestion(ref controller, result.Model, step.Formula, ref items, step.Situation);
+                // TODO: Resolve recht/geen recht.
+                if (!string.IsNullOrEmpty(step.Break))
+                {
+                    ContentNode node = new ContentNode(string.Join('.',new[] { "geen_recht",step.Situation,step.Formula }.Where(s => !string.IsNullOrEmpty(s)))) { IsBreak = true,IsSituational=step.IsSituational,Situation=step.Situation, Parameter = new Parameter(name:"recht",value:null,type:TypeEnum.Boolean,model:ref model) };
+                    items.Add(node);
+                }
+            }
+
+
+            Assert.True(items.Count == 28);
+            // TODO: put flat item list with nodes in a multibranch dependency tree...
+            //var serializer = new YamlDotNet.Serialization.Serializer();
+            //var yaml = serializer.Serialize(items);
+
+            //List<string> tokens = new List<string>();
+
+            //foreach (var item in items)
+            //{
+            //    tokens.Add(item.Name);
+            //}
+            //var tokenYaml = serializer.Serialize(tokens);
+
         }
     }
 }
