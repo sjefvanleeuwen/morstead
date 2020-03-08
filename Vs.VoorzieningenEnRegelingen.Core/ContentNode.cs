852c783a3cbf27e94b825768c70761b9110bc42c
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-03-08 15:06:50 +0100

    Adding Content Node For Content tree

---
+++ Vs.VoorzieningenEnRegelingen.Core/ContentNode.cs
@@ -0,0 +1,25 @@
+﻿using Vs.Core.Collections.NodeTree;
+using Vs.VoorzieningenEnRegelingen.Core.Model;
+
+namespace Vs.VoorzieningenEnRegelingen.Core
+{
+    public class ContentNode : TreeNodeBase<ContentNode>
+    {
+        public bool IsSituational { get; set; }
+        public bool IsBreak { get; set; }
+        public IParameter Parameter { get; set; }
+        public string Situation { get; set; }
+        
+        public ContentNode(string name) : base(name)
+        {
+           
+        }
+
+        protected override ContentNode MySelf
+        {
+            get { return this; }
+        }
+    }
+}
+
+
