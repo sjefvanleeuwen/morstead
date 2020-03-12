8381efa87cbf0df9a77a734986248c42d4b74842
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-03-12 19:31:58 +0100

    Content Collection (WIP)

---
+++ Vs.VoorzieningenEnRegelingen.Core/Model/Content/ContentCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vs.Graph.Core.Data;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+
+namespace Vs.VoorzieningenEnRegelingen.Core.Model.Content
+{
+    public class ContentCollection : List<ContentItem>, ISerialize
+    {
+        public void Read(IParser parser, Type expectedType, ObjectDeserializer nestedObjectDeserializer)
+        {
+            var o = (dynamic)nestedObjectDeserializer(typeof(object));
+            foreach (var item in o["content"])
+            {
+                this.Add(new ContentItem() {SemanticKey=item[""] });
+            }
+        }
+
+        public void Write(IEmitter emitter, ObjectSerializer nestedObjectSerializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
