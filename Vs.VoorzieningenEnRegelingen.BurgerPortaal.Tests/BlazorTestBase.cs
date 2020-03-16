e61368014b04474ad487c9f5819e3e14a0e12e94
Marijn Pessers <marijn@pessers.net>
2020-03-16 14:45:17 +0100

    Added testing for Blazor components

---
+++ Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests/BlazorTestBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Testing;
+
+namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests
+{
+    public class BlazorTestBase
+    {
+        protected TestHost _host;
+
+        public BlazorTestBase()
+        {
+            _host = new TestHost();
+        }
+    }
+}
---
+++ library/Microsoft.AspNetCore.Components.Testing/readme.md
@@ -0,0 +1,4 @@
+﻿This is not yet part of .Net and could only be part of .Net5.
+This was gotten from 
+https://github.com/SteveSandersonMS/BlazorUnitTestingPrototype
+on 16-3-2020
