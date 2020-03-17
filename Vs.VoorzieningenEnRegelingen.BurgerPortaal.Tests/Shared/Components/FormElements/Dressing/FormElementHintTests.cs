149b85a81375bd01bb364678d04e4975060b558b
Marijn Pessers <marijn@pessers.net>
2020-03-17 16:17:55 +0100

    FormElementHint Tests implemented

--- Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests/Shared/Components/FormElements/Dressing/FormElementHintTests.cs
+++ Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests/Shared/Components/FormElements/Dressing/FormElementHintTests.cs
@@ -6,42 +6,44 @@ using Vs.VoorzieningenEnRegelingen.BurgerPortaal.Shared.Components.FormElements.
 
 namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests.Shared.Components
 {
-    public class ErrorTests : BlazorTestBase
+    public class FormElementHintTests : BlazorTestBase
     {
         [Fact]
-        public void ErrorEmpty()
+        public void FormElementHintEmpty()
         {
-            var component = _host.AddComponent<Error>();
+            var component = _host.AddComponent<FormElementHint>();
             Assert.Empty(component.GetMarkup());
             var variables = new Dictionary<string, object> {
                 { "Text", " " }
             };
-            component = _host.AddComponent<Error>(variables);
+            component = _host.AddComponent<FormElementHint>(variables);
             Assert.Empty(component.GetMarkup());
         }
 
         [Fact]
-        public void ErrorExists()
+        public void FormElementHintExists()
         {
             var variables = new Dictionary<string, object> {
+                { "Name", "DeNaam" },
                 { "Text", "Error text" }
             };
-            var component = _host.AddComponent<Error>(variables);
+            var component = _host.AddComponent<FormElementHint>(variables);
             Assert.NotEmpty(component.GetMarkup());
-            Assert.NotNull(component.Find("div"));
-            Assert.Equal("Error text", component.Find("div").InnerHtml);
+            Assert.NotNull(component.Find("span"));
+            Assert.Equal("hint_DeNaam", component.Find("span").Attr("id"));
+            Assert.Equal("Error text", component.Find("span").InnerHtml);
         }
 
         [Fact]
-        public void ErrorExistsMarkup()
+        public void FormElementHintExistsMarkup()
         {
             var variables = new Dictionary<string, object> {
                 { "Text", "Error<i> text</i>" }
             };
-            var component = _host.AddComponent<Error>(variables);
+            var component = _host.AddComponent<FormElementHint>(variables);
             Assert.NotEmpty(component.GetMarkup());
-            Assert.NotNull(component.Find("div"));
-            Assert.Equal(" text", component.Find("div > i").InnerHtml);
+            Assert.NotNull(component.Find("span"));
+            Assert.Equal(" text", component.Find("span > i").InnerHtml);
         }
     }
 }
