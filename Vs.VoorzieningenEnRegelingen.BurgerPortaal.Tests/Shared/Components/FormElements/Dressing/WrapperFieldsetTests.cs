c8398d38a5c806122ddf77315cbb5db0816165c1
Marijn Pessers <marijn@pessers.net>
2020-03-18 07:40:01 +0100

    WrapperFieldsetTests implemented

---
+++ Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests/Shared/Components/FormElements/Dressing/WrapperFieldsetTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Vs.Core.Extensions;
+using Xunit;
+using Vs.VoorzieningenEnRegelingen.BurgerPortaal.Shared.Components.FormElements.Dressing;
+
+namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests.Shared.Components
+{
+    public class WrapperFieldsetTests : BlazorTestBase
+    {
+        [Fact]
+        public void WrapperFieldsetEmpty()
+        {
+            var component = _host.AddComponent<WrapperFieldset>();
+            Assert.True(string.IsNullOrWhiteSpace(component.Find("fieldset").InnerHtml));
+            Assert.Equal("input input--invalid", component.Find("fieldset").Attr("class"));
+        }
+
+        [Fact]
+        public void WrapperFieldsetSet()
+        {
+            var variables = new Dictionary<string, object> {
+                { "IsValid", false }
+            };
+            var component = _host.AddComponent<WrapperFieldset>(variables);
+            Assert.Equal("input input--invalid", component.Find("fieldset").Attr("class"));
+            variables = new Dictionary<string, object> {
+                { "IsValid", true }
+            };
+            component = _host.AddComponent<WrapperFieldset>(variables);
+            Assert.Equal("input ", component.Find("fieldset").Attr("class"));
+        }
+    }
+}
