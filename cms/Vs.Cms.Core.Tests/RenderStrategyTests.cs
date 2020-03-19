d938d6150c2c472a59052621adb0bd2aa66af609
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-03-19 19:25:23 +0100

    Unit test for RenderStrategy

---
+++ cms/Vs.Cms.Core.Tests/RenderStrategyTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Vs.Cms.Core.Tests
+{
+    public class RenderStrategyTests
+    {
+        [Fact]
+        public void CanRenderSimpleTemplate()
+        {
+            var renderer = new RenderStrategy(new Liquid(), new Markdown());
+            var result = renderer.Render(@"hello {{variable}}",  new { variable = "world" } );
+            Assert.True(result == "<p>hello world</p>\n");
+        }
+
+        [Fact]
+        public void CanRenderSimpleTemplateWithMarkdown()
+        {
+            var renderer = new RenderStrategy(new Liquid(), new Markdown());
+            var result = renderer.Render(@"**hello {{variable}}**", new { variable = "world" });
+            Assert.True(result == "<p><strong>hello world</strong></p>\n");
+        }
+    }
+}
