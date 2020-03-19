fa3d4cb10e0e29b431e6b33c893a0a256d4a2c9c
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-03-19 19:24:40 +0100

    Implement Scriban and Markdig implementations for default renderer

---
+++ cms/Vs.Cms.Core/Liquid.cs
@@ -0,0 +1,14 @@
+﻿using Scriban;
+using Vs.Cms.Core.Interfaces;
+
+namespace Vs.Cms.Core
+{
+    public class Liquid : ITemplateEngine
+    {
+        public string Render(string template, dynamic model)
+        {
+            var t = Template.ParseLiquid(template);
+            return t.Render(model);
+        }
+    }
+}
---
+++ cms/Vs.Cms.Core/Markdown.cs
@@ -0,0 +1,12 @@
+﻿using Vs.Cms.Core.Interfaces;
+
+namespace Vs.Cms.Core
+{
+    public class Markdown : IMarkupLanguage
+    {
+        public string Render(string content)
+        {
+            return Markdig.Markdown.ToHtml(content);
+        }
+    }
+}
