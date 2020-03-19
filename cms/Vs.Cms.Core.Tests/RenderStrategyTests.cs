de5056552fb9efd70d27e6e00436aea8455fee69
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-03-19 21:50:42 +0100

    Test: CMS CanRenderTemplateUsingDictionary

--- cms/Vs.Cms.Core.Tests/RenderStrategyTests.cs
+++ cms/Vs.Cms.Core.Tests/RenderStrategyTests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Vs.Cms.Core.Tests
 {
@@ -18,6 +20,23 @@ namespace Vs.Cms.Core.Tests
             var renderer = new RenderStrategy(new Liquid(), new Markdown());
             var result = renderer.Render(@"**hello {{variable}}**", new { variable = "world" });
             Assert.True(result == "<p><strong>hello world</strong></p>\n");
+        }
+
+        [Fact]
+        public void CanRenderTemplateUsingDictionary()
+        {
+            var fields = new Dictionary<string,object>();
+            fields.Add("boolean",true);
+            fields.Add("double",4.25);
+            fields.Add("hello","hello");
+            var renderer = new RenderStrategy(new Liquid(), new Markdown());
+            var result = renderer.Render(@"**{{hello}}**
+€ {{double}} 
+{% if boolean %}
+  world!
+{% endif %}
+", fields);
+            Assert.True(result == "<p><strong>hello</strong>\n€ 4.25</p>\n<p>world!</p>\n");
         }
     }
 }
