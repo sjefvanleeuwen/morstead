80e63ab90ab99d0d1e47b082429f112927d8e9b9
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-03-19 19:24:03 +0100

    Adding Renderstrategy (injectable)

---
+++ cms/Vs.Cms.Core/RenderStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Vs.Cms.Core.Interfaces;
+
+namespace Vs.Cms.Core
+{
+    public class RenderStrategy : IRenderStrategy
+    {
+        private readonly ITemplateEngine templateEngine;
+        private readonly IMarkupLanguage markupLanguage;
+
+        public RenderStrategy(ITemplateEngine templateEngine, IMarkupLanguage markupLanguage)
+        {
+            this.templateEngine = templateEngine;
+            this.markupLanguage = markupLanguage;
+        }
+
+        public string Render(string template, dynamic model)
+        {
+            return markupLanguage.Render(templateEngine.Render(template, model));
+        }
+    }
+}
