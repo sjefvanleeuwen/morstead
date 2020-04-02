8cf5bc3d4efa7cfba2a96cfde87d3dce66c28072
Marijn Pessers <marijn@pessers.net>
2020-04-02 12:14:14 +0200

    SituationParameterValues added to contentNode

--- Vs.VoorzieningenEnRegelingen.Core/ContentNode.cs
+++ Vs.VoorzieningenEnRegelingen.Core/ContentNode.cs
@@ -1,4 +1,5 @@
-﻿using Vs.Core.Collections.NodeTree;
+﻿using System.Collections.Generic;
+using Vs.Core.Collections.NodeTree;
 using Vs.VoorzieningenEnRegelingen.Core.Model;
 
 namespace Vs.VoorzieningenEnRegelingen.Core
@@ -10,7 +11,7 @@ namespace Vs.VoorzieningenEnRegelingen.Core
         public IParameter Parameter { get; set; }
         public string Situation { get; set; }
         public string Name { get; private set; }
-
+        Dictionary<string, string> SituationParameterValues { get; set; }
         public ContentNode(string name)
         {
             Name = name;
