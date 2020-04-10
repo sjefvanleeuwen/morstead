4dd434fc951866f37c0000b7091356d18172a68d
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-10 15:10:23 +0200

    Adding Situations to Table Model

--- Vs.VoorzieningenEnRegelingen.Core/Model/Table.cs
+++ Vs.VoorzieningenEnRegelingen.Core/Model/Table.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Vs.Core.Diagnostics;
 
 namespace Vs.VoorzieningenEnRegelingen.Core.Model
 {
     public class Table
     {
-        public Table(DebugInfo debugInfo, string name, List<ColumnType> columnTypes, List<Row> rows)
+        public Table(DebugInfo debugInfo, string name, List<ColumnType> columnTypes, List<Row> rows, IEnumerable<ISituation> situations = null)
         {
             DebugInfo = debugInfo ?? throw new ArgumentNullException(nameof(debugInfo));
             Name = name ?? throw new ArgumentNullException(nameof(name));
             ColumnTypes = columnTypes ?? throw new ArgumentNullException(nameof(columnTypes));
             Rows = rows ?? throw new ArgumentNullException(nameof(rows));
+            Situations = situations;
             if (Rows.Count == 0) throw new ArgumentNullException(nameof(rows));
             for (int i = 0; i < ColumnTypes.Count; i++)
             {
@@ -25,5 +27,7 @@ namespace Vs.VoorzieningenEnRegelingen.Core.Model
         public string Name { get; }
         public List<ColumnType> ColumnTypes { get; }
         public List<Row> Rows { get; }
+        public IEnumerable<ISituation> Situations { get; }
+        public bool IsSituational { get { return (Situations != null && Situations.Any()); } }
     }
 }
