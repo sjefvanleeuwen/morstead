e6ab295932f01ff6c37c7591950cefc084a8b745
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-10 15:30:14 +0200

    Yaml Test File Loader

---
+++ Vs.VoorzieningenEnRegelingen.Core.TestData/YamlTestFileLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Vs.VoorzieningenEnRegelingen.Core.TestData
+{
+    public class YamlTestFileLoader
+    {
+        public static string Load(string path)
+        {
+            return File.ReadAllText($"../../../../Vs.VoorzieningenEnRegelingen.Core.TestData/bin/Debug/netstandard2.0/YamlScripts/{path}");
+        }
+    }
+}
