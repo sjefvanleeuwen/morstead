ac9aebb6327d7e7b6e5c54fe6c42baf30a48352f
Marijn Pessers <marijn@pessers.net>
2020-04-15 17:59:27 +0200

    Radio edited Radio in testdisplay

--- Vs.VoorzieningenEnRegelingen.BurgerPortaal/Pages/ElementTester.razor.cs
+++ Vs.VoorzieningenEnRegelingen.BurgerPortaal/Pages/ElementTester.razor.cs
@@ -69,5 +69,45 @@ namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Pages
         {
             FieldSelect.CustomValidate();
         }
+
+        readonly IFormElementData FieldRadio = new BooleanFormElementData
+        {
+            Label = "RadioLabel",
+            Name = "RadioName",
+            Value = "",
+            HintText = "RadioHint",
+            InferedType = Core.TypeInference.InferenceResult.TypeEnum.Boolean,
+            Options = new Dictionary<string, string>
+            {
+                { "Optie1", "Optie1" },
+                { "Optie2", "Optie2" },
+                { "SelectValue", "Optie3" },
+                { "Optie4", "Optie4" }
+            }
+        };
+
+        private void ValidateRadio()
+        {
+            FieldRadio.CustomValidate();
+        }
+
+        readonly IFormElementData FieldRadioHorizontal = new BooleanFormElementData
+        {
+            Label = "RadioHorizontalLabel",
+            Name = "RadioHorizontalName",
+            Value = "",
+            HintText = "RadioHorizontalHint",
+            InferedType = Core.TypeInference.InferenceResult.TypeEnum.Boolean,
+            Options = new Dictionary<string, string>
+            {
+                { "Ja", "Ja" },
+                { "Nee", "Nee" }
+            }
+        };
+
+        private void ValidateRadioHorizontal()
+        {
+            FieldRadioHorizontal.CustomValidate();
+        }
     }
 }
