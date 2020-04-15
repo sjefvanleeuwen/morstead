f1c1ddca84249ff672b03c9b4a32fba27bb97f6e
Marijn Pessers <marijn@pessers.net>
2020-04-15 16:30:24 +0200

    update to textfield Tester implemented

---
+++ Vs.VoorzieningenEnRegelingen.BurgerPortaal/Pages/ElementTester.razor.cs
@@ -0,0 +1,26 @@
+﻿using Vs.VoorzieningenEnRegelingen.BurgerPortaal.Objects.FormElements;
+using Vs.VoorzieningenEnRegelingen.BurgerPortaal.Objects.FormElements.Interfaces;
+
+namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Pages
+{
+    public partial class ElementTester
+    {
+        readonly ITextFormElementData FieldText = new TextFormElementData
+        {
+            Label = "TextLabel",
+            Name = "TextName",
+            Value = "TextValue",
+            HintText = "TextHint",
+            InferedType = Core.TypeInference.InferenceResult.TypeEnum.String
+        };
+
+        readonly ITextFormElementData FieldTextArea = new TextFormElementData
+        {
+            Label = "TextAreaLabel",
+            Name = "TextAreaName",
+            Value = "TextAreaValue",
+            HintText = "TextAreaHint",
+            InferedType = Core.TypeInference.InferenceResult.TypeEnum.String
+        };
+    }
+}
