3bc4fbb0966e85d37dcd9cbffe9f42335567aae9
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-16 23:43:58 +0200

    Some more best practice ideas (resource response types 404, 400

--- Vs.Rules.OpenApi/Controllers/RulesControllerV2.cs
+++ Vs.Rules.OpenApi/Controllers/RulesControllerV2.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
 using System.Threading.Tasks;
+using Vs.Rules.OpenApi.Dto.v2;
 
 namespace Vs.Rules.OpenApi.Controllers
 {
@@ -22,6 +23,35 @@ namespace Vs.Rules.OpenApi.Controllers
         public async Task<string> Ping()
         {
             return "Pong";
+        }
+
+        /// <summary>
+        /// Gets the statistics for the current configuration being executed.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns>Statistics</returns>
+        /// <response code="404">Some yaml resource(s) could not be found</response>
+        /// <response code="400">Configuration Invalid</response>
+        [HttpPost("statistics")]
+        [ProducesResponseType(typeof(ConfigurationInvalidResponse), 404)]
+        [ProducesResponseType(typeof(ConfigurationInvalidResponse), 400)]
+        public async Task<RuleStatistics> GetStatistics(RuleConfiguration configuration)
+        {
+            return new RuleStatistics();
+        }
+        /// <summary>
+        /// For debugging purposes you can parse the configuration without executing it.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns>ParesResult</returns>
+        /// <response code="404">Some yaml resource(s) could not be found</response>
+        /// <response code="400">Configuration Invalid</response>
+        [HttpPost("parse")]
+        [ProducesResponseType(typeof(ConfigurationInvalidResponse), 404)]
+        [ProducesResponseType(typeof(ConfigurationInvalidResponse), 400)]
+        public async Task<ParseResult> GetParseResults(RuleConfiguration configuration)
+        {
+            return new ParseResult();
         }
     }
 }
---
+++ Vs.Rules.OpenApi/Dto/v2/RuleStatistics.cs
@@ -0,0 +1,8 @@
+﻿namespace Vs.Rules.OpenApi.Dto.v2
+{
+    public class RuleStatistics
+    {
+        public int Steps { get; set; }
+        public int CurrentStep { get; set; }
+    }
+}
