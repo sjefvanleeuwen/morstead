c341e852c361b1dd4ba7c8b315f5f9c11b2614db
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-16 21:29:26 +0200

    Vendor: Add Rules OpenApi

---
+++ Vs.Rules.OpenApi/Controllers/RulesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Vs.Rules.OpenApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RulesController : ControllerBase
+    {
+
+    }
+}
---
+++ YamlParse/Program.cs
@@ -0,0 +1,41 @@
+﻿using CommandLine;
+using System;
+
+namespace YamlParse
+{
+    class OptionsMutuallyExclusive
+    {
+        //define commands in set(group) named web
+        [Option('c',"content",Required=true, SetName = "mode",HelpText="For parsing content yaml files")]
+        public bool UseContentParser { get; set; }
+        [Option('r', "rule", Required = true, SetName = "mode", HelpText = "For parsing rule yaml files")]
+        public bool UseRuleParser { get; set; }
+
+        //Common: the next option can be used with any set because it's not included in a set
+        [Option('f', "path", Required = true, HelpText = "Specify Yaml File")]
+        public string File { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
+
+        }
+    }
+}
