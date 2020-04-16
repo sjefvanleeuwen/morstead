4f2bfb09b94e7ace1c6c48d3b849dd167e5cebb8
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-16 22:53:49 +0200

    Fix build

--- YamlParse/Program.cs
+++ YamlParse/Program.cs
@@ -21,21 +21,7 @@ namespace YamlParse
     {
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed<Options>(o =>
-                   {
-                       if (o.Verbose)
-                       {
-                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
-                       }
-                       else
-                       {
-                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
-                           Console.WriteLine("Quick Start Example!");
-                       }
-                   });
-
+        
         }
     }
 }
