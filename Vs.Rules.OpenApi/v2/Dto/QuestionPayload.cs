51b48cfd7b76a8e91df735a5a87875c64e2eca71
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-17 18:14:38 +0200

    Adding Sparse Question Payload

---
+++ Vs.Rules.OpenApi/v2/Dto/QuestionPayload.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Vs.Rules.OpenApi.v2.Dto
+{
+    public class QuestionPayload
+    {
+        IEnumerable<IParameter> Parameters { get; set; }
+
+        public class Parameter : IParameter
+        {
+            public object Value { get; set; }
+            public string SemanticKey { get; set; }
+        }
+
+        public interface IParameter : ISemanticKey
+        {
+            object Value { get; set; }
+        }
+
+        public interface ISemanticKey
+        {
+            string SemanticKey { get; set; }
+        }
+    }
+}
