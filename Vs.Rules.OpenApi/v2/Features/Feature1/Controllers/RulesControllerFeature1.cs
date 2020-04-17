229602cd283270c9b31d4ba5456d1ede607a7344
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-17 20:08:24 +0200

    rename to feature1

--- Vs.Rules.OpenApi/v2/Features/Feature1/Controllers/RulesControllerFeature1.cs
+++ Vs.Rules.OpenApi/v2/Features/Feature1/Controllers/RulesControllerFeature1.cs
@@ -9,9 +9,9 @@ namespace Vs.Rules.OpenApi.v2.Features.Feature1.Controllers
     /// Uses best practices from: https://github.com/RicoSuter/NSwag/wiki/AspNetCoreOpenApiDocumentGenerator
     /// </summary>
     /// <seealso cref="ControllerBase" />
-    [ApiVersion("2.0-beerrun")]
+    [ApiVersion("2.0-feature1")]
     [Route("api/v{version:apiVersion}/rules")]
-    [OpenApiTag("Rules Engine", Description = "This is current api with feature 2 implementation")]
+    [OpenApiTag("Rules Engine", Description = "This is current api with feature 1 implementation")]
     [ApiController]
     public class RulesControllerFeature1 : RulesController
     {
