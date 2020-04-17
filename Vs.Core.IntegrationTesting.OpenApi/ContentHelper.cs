a0179464e01126c765c45c3039388d46e42fa228
sjefvanleeuwen <@sjefvanleeuwen>
2020-04-17 21:59:28 +0200

    Adding Vs.Core.IntegrationTest.OpenApi testing strategie

---
+++ Vs.Core.IntegrationTesting.OpenApi/ContentHelper.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace Vs.Core.IntegrationTesting.OpenApi
+{
+    public static class ContentHelper
+    {
+        public static StringContent GetStringContent(object obj)
+            => new StringContent(JsonConvert.SerializeObject(obj), Encoding.Default, "application/json");
+    }
+}
