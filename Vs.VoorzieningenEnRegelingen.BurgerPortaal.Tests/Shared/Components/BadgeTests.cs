3d6b9f56c154c86e20a2c9ff9358f49a206b6911
Marijn Pessers <marijn@pessers.net>
2020-04-22 14:27:49 +0200

    Badge Tests implemented

--- Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests/Shared/Components/BadgeTests.cs
+++ Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests/Shared/Components/BadgeTests.cs
@@ -1,35 +1,28 @@
-﻿namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests.Shared.Components
+﻿using Bunit;
+using System.Linq;
+using Vs.VoorzieningenEnRegelingen.BurgerPortaal.Shared.Components;
+using Xunit;
+
+namespace Vs.VoorzieningenEnRegelingen.BurgerPortaal.Tests.Shared.Components
 {
     public class BadgeTests : BlazorTestBase
     {
-        //[Fact]
-        //public void BadgeEmpty()
-        //{
-        //    var component = _host.AddComponent<Badge>();
-        //    Assert.Null(component.Find("span"));
-        //}
+        [Fact]
+        public void BadgeEmpty()
+        {
+            var cut = RenderComponent<Badge>();
+            Assert.Empty(cut.Nodes);
+        }
 
-        //[Theory]
-        //[InlineData(0, false)]
-        //[InlineData(null, false)]
-        //[InlineData(1, true)]
-        //public void BadgeWorksShown(int? value, bool shown)
-        //{
-        //    var variables = new Dictionary<string, object> { { "Number", value } };
-        //    var component = _host.AddComponent<Badge>(variables);
-        //    Assert.Equal(shown, component.Find("span") != null);
-        //}
-
-        //[Theory]
-        //[InlineData(5, "5", 5, "-5")]
-        //[InlineData(-5, "-5", 0, "0")]
-        //[InlineData(18, "18", -30, "30")]
-        //public void BadgeShowsCorrectly(int number, string numberValue, int translateY, string translateYValue)
-        //{
-        //    var variables = new Dictionary<string, object> { { "Number", number } };
-        //    var component = _host.AddComponent<Badge>(variables);
-        //    Assert.Equal(numberValue, component.Find("span").InnerText);
-        //    Assert.Equal($"transform: translateY({translateYValue}px);", component.Find("span").Attr("style"));
-        //}
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(null, false)]
+        [InlineData(1, true)]
+        public void BadgeWorksShown(int? value, bool shown)
+        {
+            var cut = RenderComponent<Badge>(
+                (nameof(Badge.Number), value));
+            Assert.Equal(shown, cut.Nodes.Count() != 0);
+        }
     }
 }
