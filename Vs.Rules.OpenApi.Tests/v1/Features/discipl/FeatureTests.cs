2f473e842c4de9aaad2d365ce264adc7122b2530
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-01 13:30:35 +0200

    Feature tests now under specifc feature folder.

---
+++ Vs.Rules.OpenApi.Tests/v1/Features/discipl/FeatureTests.cs
@@ -0,0 +1,24 @@
+﻿using Vs.Core.IntegrationTesting.OpenApi;
+using Xunit;
+
+namespace Vs.Rules.OpenApi.Tests.v1.Features.discipl
+{
+    public class FeatureTests : IClassFixture<TestFixture<Startup>>
+    {
+        private RulesClient Client;
+
+        public FeatureTests(TestFixture<Startup> fixture)
+        {
+            Client = new RulesClient(fixture.Client);
+        }
+
+        /*
+        [Fact]
+        async void PingShouldProvidePong()
+        {
+            var s = await Client.PingAsync();
+            Assert.True(s == "Pong from v1");
+        }
+        */
+    }
+}
