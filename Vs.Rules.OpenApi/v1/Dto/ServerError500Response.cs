6dec03dca31047f527eaba476b446189229b8bef
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-02 16:45:26 +0200

    Generic response message for 404 and 500 api errors

---
+++ Vs.Rules.OpenApi/v1/Dto/ServerError500Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vs.Rules.OpenApi.v1.Dto
+{
+    /// <summary>
+    /// Standard Server Error 500 Response
+    /// </summary>
+    public class ServerError500Response
+    {
+        /// <summary>
+        /// The endoint of the resource that could caused the error.
+        /// </summary>
+        /// <value>
+        /// The endpoint.
+        /// </value>
+        public Uri Endpoint { get; set; }
+    }
+}
