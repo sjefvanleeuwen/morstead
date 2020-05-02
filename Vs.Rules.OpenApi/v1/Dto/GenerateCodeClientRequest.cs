7714c8d3800d8de5852d394bc61fc38d7f7fe2fc
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-02 16:45:59 +0200

    Code Generator base API client request and response types

---
+++ Vs.Rules.OpenApi/v1/Dto/GenerateCodeClientRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Vs.Rules.OpenApi.v1.Dto
+{
+    public abstract class GenerateCodeClientRequest
+    {
+        public Uri SwaggerContractEndpoint { get; set; }
+    }
+}
---
+++ Vs.Rules.OpenApi/v1/Dto/GenerateCodeClientResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Vs.Rules.OpenApi.v1.Dto
+{
+    /// <summary>
+    /// Base response message for code generation to consume the API.
+    /// </summary>
+    public abstract class GenerateCodeClientResponse
+    {
+        /// <summary>
+        /// Generated client code.
+        /// </summary>
+        /// <value>
+        /// The client code.
+        /// </value>
+        public string ClientCode { get; set; }
+    }
+}
