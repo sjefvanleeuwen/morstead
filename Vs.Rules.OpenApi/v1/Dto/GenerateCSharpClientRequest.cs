c148070c764bc715568249c74e3668957b5f215b
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-02 16:46:20 +0200

    CSharp Client Generator API client request and response types

---
+++ Vs.Rules.OpenApi/v1/Dto/GenerateCSharpClientRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Vs.Rules.OpenApi.v1.Dto
+{
+    public class GenerateCSharpClientRequest : GenerateCodeClientRequest
+    {
+        public Uri Endpoint { get; set; }
+    }
+}
---
+++ Vs.Rules.OpenApi/v1/Dto/GenerateCSharpClientResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Vs.Rules.OpenApi.v1.Dto
+{
+    /// <summary>
+    /// Response message for csharp code generation to consume the API.
+    /// </summary>
+    public class GenerateCSharpClientResponse : GenerateCodeClientResponse
+    {
+    }
+}
