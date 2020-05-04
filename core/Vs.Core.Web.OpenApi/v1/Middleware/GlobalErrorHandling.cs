0e3edab2b5768ceba8307350a644b059cfe888dc
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-04 12:40:51 +0200

    Adding api global error handling extension

---
+++ core/Vs.Core.Web.OpenApi/v1/Middleware/GlobalErrorHandling.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using Vs.Core.Web.OpenApi.v1.Dto.ProtocolErrors;
+
+namespace Vs.Core.Web.OpenApi.v1.Middleware
+{
+    public static class GlobalErrorHandling
+    {
+            public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+            {
+                app.UseExceptionHandler(appError =>
+                {
+                    appError.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+
+                        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (contextFeature != null)
+                        {
+                            await context.Response.WriteAsync(new ServerError500Response()
+                            {
+                                Message = "Internal Server Error."
+                            }.ToString());
+                        }
+                    });
+                });
+            }
+        }
+}
