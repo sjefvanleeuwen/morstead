f4b55744b4e6c664f1f587561293a57b5b5c063c
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-04 19:02:36 +0200

    Adding JWT Claim Roles Dto

---
+++ core/Vs.Core.Web.OpenApi/v1/Dto/JwtToken/Role.cs
@@ -0,0 +1,7 @@
+﻿namespace Vs.Core.Web.OpenApi.v1.Dto.JwtToken
+{
+    public class Role
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ core/Vs.Core.Web.OpenApi/v1/Dto/JwtToken/Roles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Vs.Core.Web.OpenApi.v1.Dto.JwtToken
+{
+    public class Roles : IEnumerable<Role>
+    {
+        List<Role> roles = new List<Role>();
+
+        public Role this[int index]
+        {
+            get { return roles[index]; }
+            set { roles.Insert(index, value); }
+        }
+
+        public IEnumerator<Role> GetEnumerator()
+        {
+            return roles.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
