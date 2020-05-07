aa2a851f1d58ecd5cd9a22cb0c58e65f923efa2f
Marijn Pessers <marijn@pessers.net>
2020-05-07 08:19:28 +0200

    Created LayerControllerTests

---
+++ core/Vs.Core.Layers.Tests/Controllers/LayerControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Vs.Core.Layers.Controllers;
+using Vs.VoorzieningenEnRegelingen.Core.TestData;
+using Xunit;
+
+namespace Vs.Core.Layers.Tests.Controllers
+{
+    public class LayerControllerTests
+    {
+        [Fact]
+        public void ShouldBeConstructed()
+        {
+            var yaml = YamlTestFileLoader.Load(@"Zorgtoeslag5.rules.yaml");
+            var sut = new LayerController(yaml);
+            Assert.True(true);
+        }
+    }
+}
