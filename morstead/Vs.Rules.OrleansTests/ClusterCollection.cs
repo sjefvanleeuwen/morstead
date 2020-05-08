5d0e525766a1606f6a93ff79f50fd92cecd599ff
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-08 21:09:52 +0200

    Test Fixture for Orleans

---
+++ morstead/Vs.Rules.OrleansTests/ClusterCollection.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Vs.Rules.OrleansTests
+{
+    [CollectionDefinition(ClusterCollection.Name)]
+    public class ClusterCollection : ICollectionFixture<ClusterFixture>
+    {
+        public const string Name = "ClusterCollection";
+    }
+}
---
+++ morstead/Vs.Rules.OrleansTests/ClusterFixture.cs
@@ -0,0 +1,22 @@
+﻿using Orleans.TestingHost;
+using System;
+
+namespace Vs.Rules.OrleansTests
+{
+    public class ClusterFixture : IDisposable
+    {
+        public ClusterFixture()
+        {
+            var builder = new TestClusterBuilder();
+            this.Cluster = builder.Build();
+            this.Cluster.Deploy();
+        }
+
+        public void Dispose()
+        {
+            this.Cluster.StopAllSilos();
+        }
+
+        public TestCluster Cluster { get; private set; }
+    }
+}
