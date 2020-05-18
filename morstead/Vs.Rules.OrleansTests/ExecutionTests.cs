b37126cef6be6bffcde0f0218c865cf94429b945
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-18 17:45:14 +0200

    update tests

--- morstead/Vs.Rules.OrleansTests/ExecutionTests.cs
+++ morstead/Vs.Rules.OrleansTests/ExecutionTests.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Threading;
 using System.Threading.Tasks;
-using Moq;
 using Orleans.TestingHost;
 using Vs.Rules.Core;
-using Vs.Rules.Core.Interfaces;
 using Vs.Rules.Grains.Interfaces;
 using Xunit;
 
 namespace Vs.Rules.OrleansTests
 {
-    [Collection(ClusterCollection.Name)]
-    public class HelloGrainTests
+
+    public class HelloGrainTests : IClassFixture<ClusterFixture>
     {
         private readonly TestCluster cluster;
 
@@ -21,13 +17,10 @@ namespace Vs.Rules.OrleansTests
         }
 
         [Fact]
-        public async Task SaysHelloCorrectly()
+        public async Task ShouldReceiveAQuestion()
         {
             var ruleWorker = this.cluster.GrainFactory.GetGrain<IRuleWorker>(0);
             var executionResult = await ruleWorker.Execute(Zorgtoeslag.Body, new ParametersCollection());
-
-            cluster.StopAllSilos();
-
             Assert.NotNull(executionResult.Questions);
         }
     }
