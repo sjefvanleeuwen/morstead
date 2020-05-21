b834a6bfcb906a93d3c9a4a8c146620ac24b63fd
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-21 14:59:47 +0200

    Adding debuginfo to parse result

--- rules/Vs.Rules.Core/ParseResult.cs
+++ rules/Vs.Rules.Core/ParseResult.cs
@@ -1,9 +1,11 @@
-﻿using Vs.Rules.Core.Interfaces;
+﻿using Vs.Core.Diagnostics;
+using Vs.Rules.Core.Interfaces;
 
 namespace Vs.Rules.Core
 {
     public class ParseResult : IParseResult
     {
+        public DebugInfo DebugInfo { get; set; }
         public bool IsError { get; set; }
         public string Message { get; set; }
         public string ExpressionTree { get; set; }
