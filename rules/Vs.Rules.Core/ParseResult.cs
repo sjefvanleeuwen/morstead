750970366a9c8de2fc7a538e67a50b1d6ffa6a8c
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-21 18:27:34 +0200

    Rename debug info to Exceptions, for fan out multiple exceptions

--- rules/Vs.Rules.Core/ParseResult.cs
+++ rules/Vs.Rules.Core/ParseResult.cs
@@ -1,11 +1,12 @@
 ﻿using Vs.Core.Diagnostics;
+using Vs.Rules.Core.Exceptions;
 using Vs.Rules.Core.Interfaces;
 
 namespace Vs.Rules.Core
 {
     public class ParseResult : IParseResult
     {
-        public DebugInfo DebugInfo { get; set; }
+        public FormattingExceptionCollection Exceptions { get; set; }
         public bool IsError { get; set; }
         public string Message { get; set; }
         public string ExpressionTree { get; set; }
