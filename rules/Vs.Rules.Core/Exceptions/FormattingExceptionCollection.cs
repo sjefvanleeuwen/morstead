aa65fb8d77e7ed230b78bdfa0bd4381b62c7abbc
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-21 18:25:24 +0200

    Introducing Formatting Exception Collection for debugging multiple errors.

---
+++ rules/Vs.Rules.Core/Exceptions/FormattingExceptionCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vs.Rules.Core.Exceptions
+{
+    public class FormattingExceptionCollection : Exception
+    {
+        public FormattingExceptionCollection(string message, List<FormattingException> exceptions) : base(message)
+        {
+            Exceptions = exceptions;
+        }
+
+        public List<FormattingException> Exceptions { get; }
+    }
+}
