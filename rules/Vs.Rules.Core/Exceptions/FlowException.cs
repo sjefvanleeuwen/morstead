8ae911b2c2b2be77e0ae0b6c6843ff12b8ea94fb
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-21 14:58:58 +0200

    Adding header and flow formatting exceptions

---
+++ rules/Vs.Rules.Core/Exceptions/FlowException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Vs.Core.Diagnostics;
+
+namespace Vs.Rules.Core.Exceptions
+{
+    [Serializable]
+    public class FlowFormattingException : FormattingException
+    {
+        public FlowFormattingException()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FlowFormattingException(string message, DebugInfo debugInfo) : base(message, debugInfo)
+        {
+        }
+    }
+}
---
+++ rules/Vs.Rules.Core/Exceptions/FormattingException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Vs.Core.Diagnostics;
+
+namespace Vs.Rules.Core.Exceptions
+{
+    public class FormattingException : Exception
+    {
+        public DebugInfo DebugInfo { get; internal set; }
+        public FormattingException()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FormattingException(string message, DebugInfo debugInfo) : base(message)
+        {
+            DebugInfo = debugInfo;
+        }
+    }
+}
