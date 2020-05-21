7fb062307831b417eb62a81dfc61f25095d05257
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-21 15:01:38 +0200

    new Theory: Should Return Debug Information

---
+++ rules/Vs.Rules.Core.Tests/DebugTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Vs.VoorzieningenEnRegelingen.Core.TestData;
+using Xunit;
+
+namespace Vs.Rules.Core.Tests
+{
+    public class DebugTests
+    {
+        [Theory]
+        [InlineData("HeaderIncomplete.yaml")]
+        [InlineData("FlowNoDefinition.yaml")]
+        [InlineData("FlowEmptyDefinition.yaml")]
+        [InlineData("FlowAmbiguousInputs.yaml")]
+        public async void ShouldReturnDebugInformation(string yamlFile)
+        {
+            var controller = new YamlScriptController();
+            controller.QuestionCallback = (FormulaExpressionContext sender, QuestionArgs args) =>
+            {
+                // should not be called.
+                throw new Exception("Questioncallback should not be called.");
+            };
+            var result = controller.Parse(YamlTestFileLoader.Load($"Malformed/{yamlFile}"));
+            Assert.True(result.IsError);
+            Assert.NotNull(result.DebugInfo);
+        }
+    }
+}
