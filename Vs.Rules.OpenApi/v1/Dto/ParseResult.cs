2ed344126be8de13136f03cc94ae27b1b0709fb5
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-22 11:39:26 +0200

    Formatting Exceptions in API parse results.

--- Vs.Rules.OpenApi/v1/Dto/ParseResult.cs
+++ Vs.Rules.OpenApi/v1/Dto/ParseResult.cs
@@ -1,24 +1,16 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Vs.Rules.OpenApi.v1.Features.discipl.Dto.Exceptions;
 
 namespace Vs.Rules.OpenApi.v1.Dto
 {
     public class ParseResult
     {
         /// <summary>
-        /// Gets or sets a value indicating whether this instance is error.
+        /// Gets or sets the formatting exceptions.
         /// </summary>
         /// <value>
-        ///   <c>true</c> if this instance is error; otherwise, <c>false</c>.
+        /// The formatting exceptions.
         /// </value>
-        [JsonProperty("isError")]
-        public bool IsError { get; set; }
-        /// <summary>
-        /// Gets or sets the message.
-        /// </summary>
-        /// <value>
-        /// The message.
-        /// </value>
-        [JsonProperty("message")]
-        public string Message { get; set; }
+        public List<FormattingException> FormattingExceptions { get; set; }
     }
 }
