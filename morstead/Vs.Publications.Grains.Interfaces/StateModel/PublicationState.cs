450e166eab689fa5bf34d99c227f0f0d24ecb044
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-22 18:21:34 +0200

    State model for publication grain.

---
+++ morstead/Vs.Publications.Grains.Interfaces/StateModel/PublicationState.cs
@@ -0,0 +1,12 @@
+﻿using Itenso.TimePeriod;
+using System.Net.Mime;
+
+namespace Vs.Publications.Grains.Interfaces.StateModel
+{
+    public class PublicationState
+    {
+        public object Content { get; set; }
+        public ContentType ContentType {get;set;}
+        public long ContentLength { get; set; }
+    }
+}
