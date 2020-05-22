ebeb7a00655af81f476c52347f19af023000e3f3
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-22 11:41:13 +0200

    Formatting Exception Shallow serializable object for mapping to api

---
+++ Vs.Rules.OpenApi/v1/Features/discipl/Dto/Exceptions/FormattingException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Vs.Core.Diagnostics;
+
+namespace Vs.Rules.OpenApi.v1.Features.discipl.Dto.Exceptions
+{
+    public class FormattingException
+    {
+        public DebugInfo DebugInfo { get; set; }
+        public string Message { get; set; }
+        public FormattingException()
+        {
+            
+        }
+
+        public FormattingException(string message, DebugInfo debugInfo)
+        {
+            Message = message;
+            DebugInfo = debugInfo;
+        }
+    }
+}
---
+++ Vs.Rules.OpenApi/v1/Features/discipl/Dto/ExecuteRuleYamlResultTypes.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Vs.Rules.OpenApi.v1.Features.discipl.Dto
+{
+    /// <summary>
+    /// Possible Execution result types for Yaml Rule Execution
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ExecuteRuleYamlResultTypes
+    {
+        Ok,
+        NotFound,
+        SyntaxError,
+    }
+}
+
