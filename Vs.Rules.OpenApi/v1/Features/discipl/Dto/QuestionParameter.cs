e88080123d0b67d12bcd813240e58e9f932ee725
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-22 10:31:43 +0200

    Fanning out DTO parameters into Question and Answer Parameters

---
+++ Vs.Rules.OpenApi/v1/Features/discipl/Dto/QuestionParameter.cs
@@ -0,0 +1,18 @@
+﻿namespace Vs.Rules.OpenApi.v1.Features.discipl.Dto
+{
+
+    /// <summary>
+    /// Contains a question asked by the server that the client should answer.
+    /// </summary>
+    public class QuestionParameter : Parameter
+    {
+
+        /// <summary>
+        /// Indicates the type of the parameter for value type casting purposes
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public ParameterType Type { get; set; }
+    }
+}
