3a65b87d029724c6fc40af99bd2d222c7f89c4c6
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-23 18:56:59 +0200

    Check word list

---
+++ rules/network/Vs.Rules.Network.Semantic.Tests/IndexingTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Vs.Rules.Network.Semantic.Tests
+{
+    public class IndexingTests
+    {
+        [Fact]
+        public void WordsShouldNotContainSpaces()
+        {
+            foreach (var word in WordData.words)
+            {
+                Assert.False(word.Contains(' '));
+            }
+        }
+    }
+}
