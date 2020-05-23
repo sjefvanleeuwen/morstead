661399fa483c1060e62a9bb24c9fc1bb8092bd6a
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-23 13:07:38 +0200

    Expanding debug tests for bi-lingual support.

--- rules/Vs.Rules.Core.Tests/DebugTests.cs
+++ rules/Vs.Rules.Core.Tests/DebugTests.cs
@@ -27,32 +27,45 @@ namespace Vs.Rules.Core.Tests
         [InlineData("YamlMalformed1")]
         public async void ShouldReturnDebugInformation(string yamlFile)
         {
-            var controller = new YamlScriptController();
-            var result = controller.Parse(YamlTestFileLoader.Load($"Malformed/{yamlFile}.yaml"));
-            Assert.True(result.IsError);
-            Assert.NotNull(result.Exceptions);
-            Assert.NotNull(result.Message);
-            Assert.True(result.Exceptions.Exceptions.Count == 1);
-            Assert.NotNull(result.Exceptions.Exceptions[0].DebugInfo);
-            Output.WriteLine($"message: {result.Message} {result.Exceptions.Exceptions[0].DebugInfo}");
+            // IMPORTANT please the nl-NL language at the end of the collection, as other
+            // tests are primarily tested under nl-NL yaml test files.
+            foreach (var language in new[] { "nl-NL","en-US" }) {
+                Globalization.SetKeywordResourceCulture(new System.Globalization.CultureInfo(language));
+                Globalization.SetFormattingExceptionResourceCulture(new System.Globalization.CultureInfo(language));
+                var controller = new YamlScriptController();
+                var result = controller.Parse(YamlTestFileLoader.Load($"Malformed/{language}/{yamlFile}.yaml"));
+                Assert.True(result.IsError);
+                Assert.NotNull(result.Exceptions);
+                Assert.NotNull(result.Message);
+                Assert.True(result.Exceptions.Exceptions.Count == 1);
+                Assert.NotNull(result.Exceptions.Exceptions[0].DebugInfo);
+                Output.WriteLine($"{language} message: {result.Message} {result.Exceptions.Exceptions[0].DebugInfo}");
+            }
         }
 
         [Theory]
         [InlineData("RootUnknownDefinitions")]
         public async void ShouldReturnDebugCollectionInformation(string yamlFile)
         {
-            var controller = new YamlScriptController();
-            var result = controller.Parse(YamlTestFileLoader.Load($"Malformed/{yamlFile}.yaml"));
-            Assert.True(result.IsError);
-            Assert.NotNull(result.Exceptions);
-            Assert.NotNull(result.Message);
-            Output.WriteLine(result.Message);
-            foreach (var exception in result.Exceptions.Exceptions)
+            // IMPORTANT please the nl-NL language at the end of the collection, as other
+            // tests are primarily tested under nl-NL yaml test files.
+            foreach (var language in new[] { "en-US", "nl-NL" })
             {
-                Assert.NotNull(exception.Message);
-                Assert.NotNull(exception.DebugInfo);
-                Output.WriteLine($"message: {exception.Message} {exception.DebugInfo}");
+                Globalization.SetKeywordResourceCulture(new System.Globalization.CultureInfo(language));
+                Globalization.SetFormattingExceptionResourceCulture(new System.Globalization.CultureInfo(language));
+                var controller = new YamlScriptController();
+                var result = controller.Parse(YamlTestFileLoader.Load($"Malformed/{language}/{yamlFile}.yaml"));
+                Assert.True(result.IsError);
+                Assert.NotNull(result.Exceptions);
+                Assert.NotNull(result.Message);
+                Output.WriteLine(result.Message);
+                foreach (var exception in result.Exceptions.Exceptions)
+                {
+                    Assert.NotNull(exception.Message);
+                    Assert.NotNull(exception.DebugInfo);
+                    Output.WriteLine($"{language}: {exception.Message} {exception.DebugInfo}");
 
+                }
             }
         }
     }
