694c4fcbf7bf68ae28417525fc612d4c027dc204
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-23 22:03:26 +0200

    Add In Memory Rule Database (NMemory)

---
+++ rules/network/Vs.Rules.Network.Semantic/RuleDatabase.cs
@@ -0,0 +1,42 @@
+﻿using NMemory;
+using NMemory.Tables;
+using System.Collections.Generic;
+using Vs.Core.Diagnostics;
+using Vs.Rules.Core.Model;
+
+namespace Vs.Rules.Network.Semantic
+{
+    public class ChoiceEntity : Choice, IChoice
+    {
+        public int Pk { get; set; }
+    }
+
+    public class StepEntity
+    {
+        public int Pk { get; set; }
+
+        public int Key { get; set; }
+
+        public string Name { get; set; }
+
+        public string Situation { get; set; }
+
+        public string SemanticKey { get; set; }
+
+    }
+
+    public class RuleDatabase : Database
+    {
+        public ITable<ChoiceEntity> Choices { get; private set; }
+        public ITable<StepEntity> Steps { get; private set; }
+
+        public RuleDatabase()
+        {
+            var choiceTable = Tables.Create(p => p.Pk, new IdentitySpecification<ChoiceEntity>(x => x.Pk, 1, 1));
+            var stepTable = Tables.Create(p => p.Pk, new IdentitySpecification<StepEntity>(x => x.Pk, 1, 1));
+            // new RedBlackTreeIndexFactory<Person>(), p => p.GroupId);
+            Choices = choiceTable;
+            Steps = stepTable;
+        }
+    }
+}
