7a9ab84d5f64d655bda649af25ee3deebe8b34cb
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-23 13:07:01 +0200

    Simple smart format test

--- rules/Vs.Rules.Core.Tests/GlobalizationTests.cs
+++ rules/Vs.Rules.Core.Tests/GlobalizationTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using SmartFormat;
+using System.Dynamic;
+using System.Globalization;
 using Vs.VoorzieningenEnRegelingen.Core.TestData;
 using Xunit;
 
@@ -19,6 +21,15 @@ namespace Vs.Rules.Core.Tests
             YamlRuleParser parser = new YamlRuleParser(YamlTestFileLoader.Load($"Globalization/rule.{culture}.yaml"), null);
             Assert.NotEmpty(parser.Flow());
             Assert.NotNull(parser.Header());
+        }
+
+        public void ShouldSmartFormatFieldsFromDifferentOjects()
+        {
+            dynamic keywords = new ExpandoObject();
+            keywords.header = "stuurinformatie";
+            var field = "a field";
+            var result = Smart.Format("Hello {header} {field}.", new { keywords.header, field });
+            Assert.Equal(result, "Hello stuurinformatie a field.");
         }
     }
 }
