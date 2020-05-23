18ce54d689a760647e3afcea50c760d2438096f3
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-23 13:07:59 +0200

    Header Formatting Exceptions are now multi lingual

--- morstead/Vs.Publications.Grains.Interfaces/IPublicationGrain.cs
+++ morstead/Vs.Publications.Grains.Interfaces/IPublicationGrain.cs
@@ -1,6 +1,11 @@
-﻿namespace Vs.Publications.Grains.Interfaces
+﻿using Orleans;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Vs.Publications.Grains.Interfaces
 {
-    public interface IPublicationGrain
+    public interface IPublicationGrain : IGrainWithStringKey
     {
+        Task Create(ContentType contentType, object Content);
     }
 }
