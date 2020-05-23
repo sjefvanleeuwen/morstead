35f9590e335dd54571af19c980349520fc4cb204
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-23 10:30:34 +0200

    Theory testing the globalized rule set.

---
+++ rules/Vs.Rules.Core.Tests/GlobalizationTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Vs.VoorzieningenEnRegelingen.Core.TestData;
+using Xunit;
+
+namespace Vs.Rules.Core.Tests
+{
+    /// <summary>
+    /// Unit tests for globalization
+    /// </summary>
+    public class GlobalizationTests
+    {
+        [Theory]
+        [InlineData("nl-NL")] // IMPORTANT please keep this at top as other unit tests by default use the nl-NL test files as their default culture.
+        [InlineData("en-US")]
+        public void ShouldReturnCorrectCultureForKeywords(string culture)
+        {
+            Globalization.SetKeywordResourceCulture(new CultureInfo(culture));
+            YamlRuleParser parser = new YamlRuleParser(YamlTestFileLoader.Load($"Globalization/rule.{culture}.yaml"), null);
+        }
+    }
+}
