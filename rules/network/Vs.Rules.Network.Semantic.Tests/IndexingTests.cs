6b5103fc502dad491de14c32702f9740044b05d1
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-23 22:04:44 +0200

    WIP Indexen Rule Model to In Memory Database Test

--- rules/network/Vs.Rules.Network.Semantic.Tests/IndexingTests.cs
+++ rules/network/Vs.Rules.Network.Semantic.Tests/IndexingTests.cs
@@ -1,16 +1,39 @@
-﻿using Xunit;
+﻿using Mapster;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Vs.Rules.Core;
+using Vs.Rules.Core.Model;
+using Vs.VoorzieningenEnRegelingen.Core.TestData;
+using Xunit;
 
 namespace Vs.Rules.Network.Semantic.Tests
 {
     public class IndexingTests
     {
         [Fact]
-        public void WordsShouldNotContainSpaces()
+        public void ShouldIndexModel()
         {
-            foreach (var word in WordData.words)
+            var controller = new YamlScriptController();
+            var result = controller.Parse(YamlTestFileLoader.Load(@"Zorgtoeslag5.yaml"));
+            Assert.False(result.IsError);
+
+            RuleDatabase db = new RuleDatabase();
+
+            foreach (var step in result.Model.Steps)
             {
-                Assert.False(word.Contains(' '));
+                db.Steps.Insert(step.Adapt<StepEntity>());
+                if (step.Choices == null)
+                    continue;
+                foreach (var choice in step.Choices)
+                {
+                    db.Choices.Insert(choice.Adapt<ChoiceEntity>());
+                }
             }
+
+            Assert.NotEmpty(db.Steps);
+            Assert.NotEmpty(db.Choices);
         }
+ 
     }
 }
