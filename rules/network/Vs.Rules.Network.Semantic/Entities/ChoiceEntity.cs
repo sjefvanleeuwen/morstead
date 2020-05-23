90b11e38c3eaf2f10090068ef9964066be50f6d1
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-24 00:19:35 +0200

    adding PkStep property for relationship between Choice and Step.

--- rules/network/Vs.Rules.Network.Semantic/Entities/ChoiceEntity.cs
+++ rules/network/Vs.Rules.Network.Semantic/Entities/ChoiceEntity.cs
@@ -5,5 +5,6 @@ namespace Vs.Rules.Network.Semantic.Entities
     public class ChoiceEntity : Choice, IChoice
     {
         public int Pk { get; set; }
+        public int PkStep { get; set; }
     }
 }
