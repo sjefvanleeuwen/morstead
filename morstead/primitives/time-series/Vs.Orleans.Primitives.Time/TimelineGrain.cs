f1251d6e39ffca2aed919157510c6a20d3305691
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-24 17:07:36 +0200

    Addinfg Morstead Time Primitive Grains

---
+++ morstead/primitives/time-series/Vs.Orleans.Primitives.Time/TimelineGrain.cs
@@ -0,0 +1,14 @@
+﻿using Itenso.TimePeriod;
+using System.Threading.Tasks;
+using Vs.Orleans.Primitives.Time.Interfaces;
+
+namespace Vs.Orleans.Primitives.Time
+{
+    public class TimelineGrain : ITimelineGrain
+    {
+        public Task AddMoment(ITimelineParticipant grain, ITimeLineMoment moment)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
