6e12ffe3cf93dbdd1beb63b47f8b80a9202a413a
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-26 15:47:02 +0200

    Add JWT TO Startup (WIP)

--- core/Vs.Core.Web.OpenApi/Startup.cs
+++ core/Vs.Core.Web.OpenApi/Startup.cs
@@ -18,7 +18,9 @@ namespace Vs.Core.Web.OpenApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddJwt(Configuration);
             services.AddOpenApiServices();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
