1b74e0a3e77d484d9e3fd725cbd635d32741ab13
sjefvanleeuwen <@sjefvanleeuwen>
2020-05-27 11:29:15 +0200

    Adding debug specific api request and response types.

---
+++ Vs.Rules.OpenApi/v1/Features/discipl/Dto/DebugRuleYamlFromContentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Vs.Rules.OpenApi.v1.Features.discipl.Dto
+{
+    /// <summary>
+    /// Execute a rule by passing a yaml and evaluating it against a collection of client parameters.
+    /// </summary>
+    /// <seealso cref="Vs.Rules.OpenApi.v1.Features.discipl.Dto.ExecuteRuleYamlRequestBase" />
+    public class DebugRuleYamlFromContentRequest : DebugRuleYamlRequestBase
+    {
+        public string Yaml { get; set; }
+    }
+}
