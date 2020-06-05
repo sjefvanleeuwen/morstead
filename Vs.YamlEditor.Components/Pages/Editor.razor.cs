ade6fb7e79e3393c997ff781f766bc53d8a3069a
Marijn Pessers <marijn@pessers.net>
2020-06-05 17:44:11 +0200

    Moved all submit functionality into a Controller

--- Vs.YamlEditor.Components/Pages/Editor.razor.cs
+++ Vs.YamlEditor.Components/Pages/Editor.razor.cs
@@ -1,12 +1,7 @@
-﻿using BlazorMonaco.Bridge;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using Vs.YamlEditor.Components.Controllers.ApiCalls;
+using Vs.YamlEditor.Components.Controllers;
 
 namespace Vs.YamlEditor.Components.Pages
 {
@@ -14,28 +9,21 @@ namespace Vs.YamlEditor.Components.Pages
     {
         private string Url { get; set; } = "https://raw.githubusercontent.com/sjefvanleeuwen/virtual-society-urukagina/master/Vs.VoorzieningenEnRegelingen.Core.TestData/YamlScripts/Zorgtoeslag5.yaml";
         private string Value { get; set; }
-        private string TypeOfContent { get; set; }
-        private CancellationTokenSource TokenSource { get; set; }
-
-        private TimeSpan _submitWait = TimeSpan.FromMilliseconds(5000);
-
-        private readonly IDictionary<string, bool> _types = new Dictionary<string, bool> {
-            { "Rule", true },
-            { "Content", false },
-            { "Routing", false },
-            { "Layer", false }
-        };
-
-        private Shared.YamlEditor _yamlEditor { get; set; }
-
-        private bool GetEnabledForType(string type)
-        {
-            return _types.ContainsKey(type) && _types[type];
+        private ValidationController _validationController;
+        private ValidationController ValidationController {
+            get
+            {
+                if (_validationController == null)
+                {
+                    _validationController = new ValidationController();
+                }
+                return _validationController;
+            }
         }
 
         private string GetStyleForType(string type)
         {
-            if (GetEnabledForType(type))
+            if (ValidationController.GetEnabledForType(type))
             {
                 return string.Empty;
             }
@@ -66,108 +54,12 @@ Details:
                 }
             }
 
-            await _yamlEditor.SetValue(Value);
+            await ValidationController.YamlEditor.SetValue(Value);
         }
 
-        private void StartSubmitCountdown()
+        public void StartSubmitCountdown()
         {
-            if (TokenSource != null)
-            {
-                TokenSource.Cancel();
-            }
-            TokenSource = new CancellationTokenSource();
-            var ct = TokenSource.Token;
-            var task = Task.Run(() =>
-            {
-                Thread.Sleep(_submitWait);
-                ct.ThrowIfCancellationRequested();
-                SubmitPage();
-            },
-            TokenSource.Token);
-        }
-
-        private async void SubmitPage()
-        {
-            if (string.IsNullOrWhiteSpace(TypeOfContent))
-            {
-                return;
-            }
-
-            var client = new RulesControllerDisciplClient(new HttpClient())
-            {
-                BaseUrl = "https://localhost:44391/"
-            };
-
-            //DebugRuleYamlFromContentResponse response = null;
-            IEnumerable<FormattingException> formattingExceptions = new List<FormattingException>();
-            try
-            {
-                var response = await client.DebugRuleYamlContentsAsync(new DebugRuleYamlFromContentRequest { Yaml = await _yamlEditor.GetValue() });
-                formattingExceptions = response.ParseResult.FormattingExceptions;
-            }
-            catch (ApiException ex)
-            {
-                if (ex.StatusCode != 404)
-                {
-                    throw ex;
-                }
-                await _yamlEditor.ResetDeltaDecorations();
-                return;
-            }
-
-            await SetDeltaDecorationsFromExceptions(formattingExceptions);
-        }
-
-        private async Task SetDeltaDecorationsFromExceptions(IEnumerable<FormattingException> formattingExceptions)
-        {
-            await _yamlEditor.ResetDeltaDecorations();
-
-            var deltaDecorations = new List<ModelDeltaDecoration>();
-            foreach (var exception in formattingExceptions)
-            {
-                var message = exception.Message;
-                var range = new BlazorMonaco.Bridge.Range()
-                {
-                    StartLineNumber = exception.DebugInfo.Start.Line,
-                    StartColumn = exception.DebugInfo.Start.Col,
-                    EndLineNumber = exception.DebugInfo.End.Line,
-                    EndColumn = exception.DebugInfo.End.Col
-                };
-
-                deltaDecorations.Add(await BuildDeltaDecoration(range, message));
-            }
-
-            await _yamlEditor.SetDeltaDecoration(deltaDecorations);
-        }
-
-        private async Task<ModelDeltaDecoration> BuildDeltaDecoration(BlazorMonaco.Bridge.Range range, string message)
-        {
-            var isWholeLine = false;
-
-            range.StartLineNumber = Math.Max(range.StartLineNumber, 1);
-            range.StartColumn = Math.Max(range.StartColumn, 1);
-            range.EndLineNumber = Math.Max(range.EndLineNumber, 1);
-            if (range.EndColumn == 0)
-            {
-                range.EndColumn = range.StartColumn;
-                var content = await _yamlEditor.GetValue();
-                var contentLines = content.Split("\n");
-                range.EndColumn = (contentLines.ElementAt(Math.Min(contentLines.Length - 1, range.EndLineNumber - 1))?.Trim().Length ?? 0) + 1;
-                isWholeLine = true;
-            }
-
-            var options = new ModelDecorationOptions
-            {
-                IsWholeLine = isWholeLine,
-                InlineClassName = "editorError",
-                InlineClassNameAffectsLetterSpacing = false,
-                ClassName = "editorError",
-                HoverMessage = new MarkdownString[] { new MarkdownString { Value = $"**Error**\r\n\r\n{message}" } },
-                GlyphMarginClassName = "editorErrorGlyph",
-                GlyphMarginHoverMessage = new MarkdownString[] { new MarkdownString { Value = $"**Error**\r\n\r\n{message}" } }
-            };
-
-            return new ModelDeltaDecoration { Range = range, Options = options };
+            ValidationController.StartSubmitCountdown();
         }
     }
 }
