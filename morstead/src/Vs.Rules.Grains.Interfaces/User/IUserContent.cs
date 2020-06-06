95b3bd7ed0fab5b64dd0f714277a36944062b2d5
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-06 17:10:59 +0200

    User Accounts Interfaces and State Objects

---
+++ morstead/src/Vs.Rules.Grains.Interfaces/User/IUserContent.cs
@@ -0,0 +1,13 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+using Vs.Rules.Grains.Interfaces.Primitives.PubSub;
+
+namespace Vs.Rules.Grains.User
+{
+    public interface IUserContent : IGrainWithStringKey
+    {
+        Task<IUserAccountPersistentGrain> GetOwner();
+        Task SetOwner(IUserAccountPersistentGrain account);
+        Task ContentSaved(SubscriptionCallback callback);
+    }
+}
---
+++ morstead/src/Vs.Rules.Grains.Interfaces/User/UserAccountState.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Vs.Rules.Grains.Interfaces.User
+{
+    public class UserAccountState
+    {
+        public CultureInfo Locale { get; set; }
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+    }
+}
