4aaa0ae8e85c0cff7448db4d13a0d686daa84cc6
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-06 17:10:01 +0200

    User Content Grain

---
+++ morstead/src/Vs.Rules.Grains/Content/UserContent.cs
@@ -0,0 +1,36 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using System.Threading.Tasks;
+using Vs.Rules.Grains.Interfaces.Content;
+using Vs.Rules.Grains.Interfaces.Primitives.PubSub;
+using Vs.Rules.Grains.User;
+
+namespace Vs.Rules.Grains.Content
+{
+    public class UserContent : Grain, IUserContent
+    {
+        public IPersistentState<UserContentState> Content { get; }
+        public UserContent([PersistentState("user-content", "content-store")] IPersistentState<UserContentState> content)
+        {
+            Content = content;
+        }
+
+        public Task SetOwner(IUserAccountPersistentGrain userAccount)
+        {
+            Content.State.Owner = userAccount.GetPrimaryKeyString();
+            return Task.CompletedTask;
+        }
+
+        public Task<IUserAccountPersistentGrain> GetOwner()
+        {
+            return Task.FromResult(GrainFactory.GetGrain<IUserAccountPersistentGrain>(Content.State.Owner));
+        }
+
+        public Task ContentSaved(SubscriptionCallback callback)
+        {
+            Content.State.ContentId = callback.GrainId;
+            Content.WriteStateAsync();
+            return Task.CompletedTask;
+        }
+    }
+}
