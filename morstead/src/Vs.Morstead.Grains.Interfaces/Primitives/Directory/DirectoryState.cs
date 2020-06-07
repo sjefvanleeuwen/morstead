25c937b83afab78fafa3620e1177d7b84dc01244
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-07 19:11:51 +0200

    Grain Directory State

---
+++ morstead/src/Vs.Morstead.Grains.Interfaces/Primitives/Directory/DirectoryState.cs
@@ -0,0 +1,50 @@
+﻿using Itenso.TimePeriod;
+using System;
+using VirtualSociety.VirtualSocietyDid;
+
+namespace Vs.Morstead.Grains.Interfaces.Primitives.Directory
+{
+    public class DirectoryState
+    {
+        public NestedDictionary<string, DirectoryItem> Dir { get; set; }
+
+        public DirectoryItem CreateDir(string path)
+        {
+            var s = path.Split("/", StringSplitOptions.RemoveEmptyEntries);
+            while (!createDir(s)) { }
+            return GetDir(s);
+        }
+
+        public DirectoryItem GetDir(string[] path)
+        {
+            var s = Dir;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (!s.ContainsKey(path[i]))
+                    throw new Exception("directory does not exist");
+                s = s[path[i]];
+            }
+            return s.Value;
+        }
+
+        private bool createDir(string[] path)
+        {
+            var s = Dir;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (!s.ContainsKey(path[i]))
+                {
+                    var nested = new NestedDictionary<string, DirectoryItem>();
+                    s.Add(path[i], nested);
+                    s[path[i]].Value = new DirectoryItem();
+                    s[path[i]].Value.Created = DateTime.Now;
+                    s[path[i]].Value.Modified = DateTime.Now;
+                    s[path[i]].Value.ItemsGrainId = new Did("mstd:dir").ToString();
+                    return false;
+                }
+                s = s[path[i]];
+            }
+            return true;
+        }
+    }
+}
