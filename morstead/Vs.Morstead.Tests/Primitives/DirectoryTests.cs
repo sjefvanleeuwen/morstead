d7cc8cd1d3da33c537518c2fd2bfdc2371394c21
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-07 22:36:50 +0200

    Extending Create Directories and storing one item its directory

--- morstead/Vs.Morstead.Tests/Primitives/DirectoryTests.cs
+++ morstead/Vs.Morstead.Tests/Primitives/DirectoryTests.cs
@@ -1,5 +1,12 @@
-﻿using Orleans.TestingHost;
+﻿using EasyCompressor;
+using Microsoft.AspNetCore.Connections;
+using Orleans.TestingHost;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using VirtualSociety.VirtualSocietyDid;
+using Vs.Morstead.Grains.Interfaces.Content;
 using Vs.Morstead.Grains.Interfaces.Primitives.Directory;
 using Xunit;
 
@@ -32,12 +39,35 @@ namespace Vs.Morstead.Tests.Primitives
         [Fact]
         public async void CanReadNestedDirectoriesInGrain()
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             var did = new Did("mstd:dir").ToString();
             var dir = cluster.GrainFactory.GetGrain<IDirectoryGrain>(did);
             var dir1 = await dir.CreateDirectory("//test1/test2/test3");
             Assert.StartsWith("did:vsoc:mstd:dir", dir1.ItemsGrainId);
             var dir2 = await dir.GetDirectory("//test1/test2//test3");
             Assert.Equal(dir1.ItemsGrainId, dir2.ItemsGrainId);
+            // Add an item to dir2
+            var contents = cluster.GrainFactory.GetGrain<IDirectoryContentsGrain>(dir2.ItemsGrainId);
+            var contentDid = new Did("mstd:pub").ToString();
+            var content = cluster.GrainFactory.GetGrain<IContentPersistentGrain>(contentDid);
+            await content.Save(new System.Net.Mime.ContentType("text/html"), Encoding.UTF8, "Unit Test, Hello World");
+            await contents.AddItem(new DirectoryContentsItem()
+            {
+                GrainId = contentDid,
+                Interface = typeof(IContentPersistentGrain)
+            });
+
+            var contents2 = cluster.GrainFactory.GetGrain<IDirectoryContentsGrain>(dir2.ItemsGrainId);
+            var list = await contents2.ListItems();
+            Assert.Single(list.Items);
+            Assert.Equal(list.Items.ElementAt(0).Value.GrainId, contentDid);
+            // tested in another unit test, but to be thorough
+            var content2 = cluster.GrainFactory.GetGrain<IContentPersistentGrain>(contentDid);
+            var content2state = await content2.Load();
+            var contentsDocument = content2state.Encoding.GetString(content2state.Content);
+            Assert.Equal("Unit Test, Hello World", contentsDocument);
+            stopwatch.Stop();
         }
     }
 }
