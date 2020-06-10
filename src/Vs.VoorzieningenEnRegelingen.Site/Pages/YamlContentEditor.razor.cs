2d27aa56b71ec7d1a13e8a1b0ec68d6c0a7395f8
Marijn Pessers <marijn@pessers.net>
2020-06-10 14:23:38 +0200

    "Save" functionality

--- src/Vs.VoorzieningenEnRegelingen.Site/Pages/YamlContentEditor.razor.cs
+++ src/Vs.VoorzieningenEnRegelingen.Site/Pages/YamlContentEditor.razor.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Vs.VoorzieningenEnRegelingen.Site.Model;
 using Vs.YamlEditor.Components.Controllers;
@@ -25,13 +28,13 @@ namespace Vs.VoorzieningenEnRegelingen.Site.Pages
         public INode BaseNode { get; set; }
 
         [Inject]
-        protected IJSRuntime JSRuntime { get; set; }        
+        protected IJSRuntime JSRuntime { get; set; }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
-                await JSRuntime.InvokeAsync<object>("split",new object[] { DotNetObjectReference.Create(this), "InvokeLayout" }).ConfigureAwait(false);
+                await JSRuntime.InvokeAsync<object>("split", new object[] { DotNetObjectReference.Create(this), "InvokeLayout" }).ConfigureAwait(false);
             }
         }
 
@@ -49,6 +52,26 @@ namespace Vs.VoorzieningenEnRegelingen.Site.Pages
         public Task Layout()
         {
             return ValidationController.YamlEditor.Layout();
+        }
+
+        private string Name { get; set; }
+
+        private IList<YamlFileInfo> YamlFileInfos { get; set; } = new List<YamlFileInfo>();
+
+        public async Task Save()
+        {
+            var content = await ValidationController.YamlEditor.GetValue().ConfigureAwait(false);
+
+            var yamlFileInfo = new YamlFileInfo
+            {
+                Name = Name.Trim(),
+                Content = content
+            };
+            if (YamlFileInfos.Any(y => y.Name == Name))
+            {
+                //name already exists
+            }
+            YamlFileInfos.Add(yamlFileInfo);
         }
     }
 }
