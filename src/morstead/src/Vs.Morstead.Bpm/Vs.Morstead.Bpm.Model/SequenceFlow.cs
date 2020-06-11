3ab913b10ad2d6c7e3f64ccc8f13522f033e1982
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-11 23:34:46 +0200

    BPMN SequenceFlow

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model/SequenceFlow.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+using Vs.Morstead.Bpm.Model.Events;
+using Vs.Morstead.Bpm.Model.Tasks;
+
+namespace Vs.Morstead.Bpm.Model
+{
+    public class SequenceFlow
+    {
+        private readonly JToken _process;
+        private readonly StartEvent _startEvent;
+        private readonly EndEvent _endEvent;
+        private string _currentStepId;
+        private JToken _current;
+
+        public SequenceFlow(JToken process, StartEvent startEvent, EndEvent endEvent)
+        {
+            _process = process;
+            _startEvent = startEvent;
+            _endEvent = endEvent;
+            _currentStepId = _startEvent.Outgoing;
+        }
+
+        public JToken Current()
+        {
+            return _current;
+        }
+
+        public ITask Next()
+        {
+            _current = _process["bpmn:sequenceFlow"].Single(p => p.Value<string>("@id") == _currentStepId);
+            var target = _current["@targetRef"].Value<string>();
+            var task = new Task(_process, target);
+            _currentStepId = task.Outgoing;
+            return task;
+        }
+    }
+}
