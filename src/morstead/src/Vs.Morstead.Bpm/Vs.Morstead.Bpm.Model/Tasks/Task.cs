16897275cac27aa9d90a595942a5f34c08a09540
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-11 23:34:25 +0200

    BPMN Task

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model/Tasks/Task.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace Vs.Morstead.Bpm.Model.Tasks
+{
+    public class Task : ITask
+    {
+        private readonly JToken o;
+
+        public string Id => o["@id"].Value<string>();
+        public string Name => o["@name"].Value<string>();
+        public string Incoming => o["bpmn:incoming"].Value<string>();
+        public string Outgoing => o["bpmn:outgoing"].Value<string>();
+
+        public Task(JToken process, string id)
+        {
+            this.o = process["bpmn:task"].Where(p => p.Value<string>("@id") == id).Single();
+        }
+    }
+}
