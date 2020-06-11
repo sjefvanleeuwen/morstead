4f2e5e94290a650c114c53dd0727c8a187aeb153
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-11 18:32:10 +0200

    Add BPMN Test Data

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.TestData/TestFileLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Vs.Morstead.Bpm.TestData
+{
+    public class TestFileLoader
+    {
+        public static string Load(string path)
+        {
+            var file = $"Vs.Morstead.Bpm.TestData/{path}";
+            if (File.Exists($"../../../../{file}"))
+            {
+                return File.ReadAllText($"../../../../{file}");
+            }
+            else
+            {
+                return File.ReadAllText(FindDocument(file));
+            }
+        }
+
+        private static string FindDocument(string file)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (File.Exists(file))
+                {
+                    return file;
+                }
+                file = $"../{file}";
+            }
+            throw new Exception("File not found.");
+        }
+    }
+}
