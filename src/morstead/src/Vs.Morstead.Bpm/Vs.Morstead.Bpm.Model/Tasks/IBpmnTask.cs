c12bb2b3129ec91f5cee70f193743439da0be1a2
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-12 22:27:29 +0200

    Bpmn Task Interface

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model/Tasks/IBpmnTask.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Vs.Morstead.Bpm.Model.Tasks
+{
+    public interface IBpmnTask
+    {
+        List<ExecutionListener> ExecutionListeners { get; set; }
+        string Id { get;  }
+        string Name { get;  }
+        string Incoming { get; }
+        string Outgoing { get; }
+    }
+}
