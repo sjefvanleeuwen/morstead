8e5cb6d70f79f6ec366cde24aa80df285947a6e6
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-14 00:35:17 +0200

    Adding Flee Context To Bpmn Process

--- src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model/BpmnProcess.cs
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model/BpmnProcess.cs
@@ -1,7 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using Flee.PublicTypes;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Diagnostics;
 using System.Xml.Linq;
+using Vs.Morstead.Bpm.Core;
 using Vs.Morstead.Bpm.Model.Events;
 
 namespace Vs.Morstead.Bpm.Model
@@ -9,7 +10,7 @@ namespace Vs.Morstead.Bpm.Model
     public class BpmnProcess
     {
         JObject o { get; set; }
-
+        public ExpressionContext Context { get; set; }
         private JToken process;
         public string Id { get; set; }
         public StartEvent StartEvent { get; }
@@ -19,12 +20,13 @@ namespace Vs.Morstead.Bpm.Model
         public BpmnProcess(string bpmn)
         {
             var doc = XDocument.Parse(bpmn);
+            Context = new ExpressionContext();
             o = JObject.Parse(JsonConvert.SerializeXNode(doc));
             process = o["bpmn:definitions"]["bpmn:process"];
             Id = process["@id"].Value<string>();
             StartEvent = new StartEvent(process);
             EndEvent = new EndEvent(process);
-            SequenceFlow = new SequenceFlow(process, StartEvent, EndEvent);
+            SequenceFlow = new SequenceFlow(process, StartEvent, EndEvent, Context);
         }
     }
 }
