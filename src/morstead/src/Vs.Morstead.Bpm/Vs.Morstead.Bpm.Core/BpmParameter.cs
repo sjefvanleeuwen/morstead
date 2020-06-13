09e98a906acd7c287b38ba7976b862347c2ffdfc
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-13 17:17:44 +0200

    Bpm Parameter collection for process state.

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Core/BpmParameter.cs
@@ -0,0 +1,8 @@
+﻿namespace Vs.Morstead.Bpm.Core
+{
+    public class BpmParameter
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Core/IBpmParametersCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Vs.Morstead.Bpm.Core
+{
+    public class BpmParametersCollection : Dictionary<string, BpmParameter>
+    {
+    }
+}
