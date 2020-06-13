6f78521e4acda4ae7c70f1ba69969d0b4dc16ce5
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-13 14:45:03 +0200

    Bpmn Test Fixture

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model.Tests/BpmnTestFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Vs.Morstead.Bpm.TestData;
+
+namespace Vs.Morstead.Bpm.Model.Tests
+{
+    public class BpmnTestFixture : IDisposable
+    {
+        public BpmnProcess Process { get; private set; }
+
+        public void LoadBpmn(string path)
+        {
+            Process = new BpmnProcess(TestFileLoader.Load(path));
+        }
+
+        public BpmnTestFixture()
+        {
+
+        }
+        public void Dispose()
+        {
+            // clean up test data
+        }
+    }
+}
