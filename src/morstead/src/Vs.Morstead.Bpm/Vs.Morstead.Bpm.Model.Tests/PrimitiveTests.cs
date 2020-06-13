4ec542062413d78354d61d679b0b97d43b119528
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-13 14:45:26 +0200

    Adding Primitive Tests for BPMN

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model.Tests/PrimitiveTests.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+using Vs.Morstead.Bpm.Model.Gateways;
+using Vs.Morstead.Bpm.TestData;
+using Xunit;
+
+namespace Vs.Morstead.Bpm.Model.Tests
+{
+    public class PrimitiveTests
+    {
+        [Fact]
+        public void ExclusiveGatewayCanRead()
+        {
+            var doc = XDocument.Parse(TestFileLoader.Load(@"Bpmn20/simple-exclusive-gateway.bpmn"));
+            string json = JsonConvert.SerializeXNode(doc);
+            JObject bpmn = JObject.Parse(json);
+            var process = bpmn["bpmn:definitions"]["bpmn:process"];
+            BpmExclusiveGateway gateway = new BpmExclusiveGateway(process, "Gateway_1d2x0fd");
+            Assert.Equal("Gateway_1d2x0fd", gateway.Id);
+            Assert.Single(gateway.Inputs);
+            Assert.Equal("Flow_0hzr3el", gateway.Inputs[0]);
+            Assert.Equal(2, gateway.Outputs.Count);
+            Assert.Equal("Flow_0gzbcgc", gateway.Outputs[0]);
+            Assert.Equal("Flow_1fg2zon", gateway.Outputs[1]);
+        }
+    }
+}
