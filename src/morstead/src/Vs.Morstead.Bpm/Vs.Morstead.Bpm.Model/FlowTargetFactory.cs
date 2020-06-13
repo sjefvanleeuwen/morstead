f537faed0953321b56fb02f5d6944736945743d2
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-13 18:21:39 +0200

    BPMN Flow target Factory

---
+++ src/morstead/src/Vs.Morstead.Bpm/Vs.Morstead.Bpm.Model/FlowTargetFactory.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+
+namespace Vs.Morstead.Bpm.Model
+{
+    public class FlowTargetFactory
+    {
+        private static string[] targets => "bpmn:task,bpmn:exclusiveGateway".Split(',');
+
+        public string Target { get; set; }
+
+        public JToken Token { get; set; }
+
+        public FlowTargetFactory(JToken process, string id)
+        {
+            foreach (var target in targets)
+            {
+                foreach (var item in process[target])
+                {
+                    switch (item.Type)
+                    {
+                        case JTokenType.Object:
+                            if (item["@id"].Value<string>() == id) {
+                                Token = item;
+                                Target = target;
+                                return;
+                            }
+                            break;
+                        case JTokenType.Property:
+                            if (item.Parent["@id"].Value<string>() == id)
+                            {
+                                Token = item.Parent;
+                                Target = target;
+                                return;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            throw new Exception($"Flow target not found, current supported are: {string.Join(",", targets)}");
+        }
+    }
+}
