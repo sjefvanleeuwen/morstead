6cf8feb1ee8a2a926aaa4e807ebb0df67bc59797
sjefvanleeuwen <@sjefvanleeuwen>
2020-06-17 12:03:15 +0200

    Rewrite directory grain to use a flat dictionary instead of an hierarchy (K,V). because of serialization issues with orleans

--- src/morstead/src/Vs.Morstead.Grains.Interfaces/Primitives/Directory/NestedDictionary.cs
+++ src/morstead/src/Vs.Morstead.Grains.Interfaces/Primitives/Directory/NestedDictionary.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.Services.Common;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Vs.Morstead.Grains.Interfaces.Primitives.Directory
