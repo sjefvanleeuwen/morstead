dc8a8578f804d4cc74eb08bd0dd9b72fb8626c5d
Marijn Pessers <marijn@pessers.net>
2020-07-03 10:42:20 +0200

    Added project BlazorMonaco

---
+++ src/temp/BlazorMonaco/Bridge/Data.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorMonaco.Bridge
+{
+    public class Position
+    {
+        public int LineNumber { get; set; }
+        public int Column { get; set; }
+    }
+
+    public class Range
+    {
+        public int StartLineNumber { get; set; }
+        public int StartColumn { get; set; }
+        public int EndLineNumber { get; set; }
+        public int EndColumn { get; set; }
+
+        public Range() { }
+
+        public Range(int startLineNumber, int startColumn, int endLineNumber, int endColumn)
+        {
+            StartLineNumber = startLineNumber;
+            StartColumn = startColumn;
+            EndLineNumber = endLineNumber;
+            EndColumn = endColumn;
+        }
+    }
+
+    public class Selection : Range
+    {
+        public int SelectionStartLineNumber { get; set; }
+        public int SelectionStartColumn { get; set; }
+        public int PositionLineNumber { get; set; }
+        public int PositionColumn { get; set; }
+    }
+
+    public class Dimension
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class StandaloneThemeData
+    {
+        public string Base { get; set; }
+        public bool? Inherit { get; set; }
+        public List<TokenThemeRule> Rules { get; set; }
+        public List<string> EncodedTokensColors { get; set; }
+        public Dictionary<string, string> Colors { get; set; }
+    }
+
+    public class TokenThemeRule
+    {
+        public string Token { get; set; }
+        public string Foreground { get; set; }
+        public string Background { get; set; }
+        public string FontStyle { get; set; }
+    }
+}
---
+++ src/temp/BlazorMonaco/Bridge/TextModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlazorMonaco.Bridge
+{
+    public class TextModel
+    {
+        public string Id { get; set; }
+        public string Uri { get; set; }
+    }
+}
